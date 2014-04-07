fff9a820d36b3e383a486fc9a47be990a8a355a7
Trizen <trizenx@gmail.com>
2014-04-07 16:28:21 +0300

    Added a new exciting script: read raw data from microphone (with ALSA/arecord). TODO: process the raw data and do something cool with it. (like executing system commands with your voice)

---
+++ raw_from_microphone.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 07 April 2014
+# Website: http://github.com/trizen
+
+# Read raw data from microphone (via ALSA/arecord)
+
+use 5.010;
+use strict;
+use warnings;
+
+use Time::HiRes qw(sleep);
+
+use constant {
+        HW_PARAMS_FILE => '/proc/asound/card0/pcm0c/sub0/hw_params',
+};
+
+open(my $pipe_h, '-|', 'arecord', '/dev/stdout') // exit $!;
+sleep 0.1;    # /proc can't be instant
+
+sub parse_config {
+    my ($file) = @_;
+
+    open my $fh, '<', $file
+      or return;
+
+    my %table;
+    while (<$fh>) {
+        if (/^([^:]+):\h*(.*\S)/) {
+            $table{$1} = $2;
+        }
+    }
+
+    close $fh;
+    return \%table;
+}
+
+# Read the hardware parameters file
+my $hw_params = parse_config(HW_PARAMS_FILE);
+
+while (read($pipe_h, (my $buffer), $hw_params->{buffer_size})) {
+
+    # Here some interesting stuff needs to be written :)
+    say length($buffer);
+}
+
+__END__
+access: MMAP_INTERLEAVED
+format: S32_LE
+subformat: STD
+channels: 2
+rate: 48000 (48000/1)
+period_size: 1024
+buffer_size: 16384
