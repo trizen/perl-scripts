ad94f2bc6e9d10b7a3ca02bf01d14af9bbbe8ca9
Trizen <trizenx@gmail.com>
2014-04-08 02:08:27 +0300

    new file:   voice_control.pl

---
+++ voice_control.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 08 April 2014
+# Website: http://github.com/trizen
+
+# Voice control - take actions based on vocal commands
+
+use utf8;
+use 5.010;
+use strict;
+use warnings;
+use List::Util qw(sum);
+
+no if $] >= 5.018, warnings => 'experimental';
+
+my %forks;
+my $config = "$ENV{HOME}/.voxforge/julius/perl.jconf";
+my @julius = qw(julius -input mic);
+
+open(my $pipe_h, '-|', @julius, '-C', $config) // exit $!;
+
+sub take_action {
+    my ($command) = @_;
+
+    given ($command) {
+        when ('<s> START MUSIC </s>') {
+            say "Starting music...";
+            push @{$forks{music}}, scalar fork();
+            if ($forks{music}[-1] == 0) {
+                exec 'mpv /home/swampyx/Muzica/*';
+            }
+        }
+        when ('<s> STOP MUSIC </s>') {
+            say "Stoping music...";
+            if (ref $forks{music} eq 'ARRAY' and @{$forks{music}} > 0) {
+                kill 1, $forks{music}[-1];
+                pop @{$forks{music}};
+            }
+        }
+        when ('<s> START TERM </s>') {
+            say "Opening the terminal...";
+        }
+        default {
+            warn "WARN: Invalid command `$command'!\n";
+        }
+    }
+}
+
+my @buffer;
+while (<$pipe_h>) {
+
+    if (!/\S/) {
+        my %conf;
+        foreach my $line (@buffer) {
+            if ($line =~ /^(\w+):\h*(.*\S)/) {
+                $conf{$1} = $2;
+            }
+        }
+
+        if (exists $conf{cmscore1} and exists $conf{sentence1}) {
+            my @vals = split(' ', $conf{cmscore1});
+            say "got: $conf{sentence1} ($conf{cmscore1})";
+            if (sum(@vals) == @vals) {    # 'cmscore1' must be: 1.000 1.000 1.000 1.000
+                take_action($conf{sentence1});
+            }
+        }
+
+        $#buffer = -1;
+    }
+
+    push @buffer, $_;
+}
+
+__END__
+pass1_best: <s> START MUSIC
+pass1_best_wordseq: 0 2 3
+pass1_best_phonemeseq: sil | y ah ng | w ah n
+pass1_best_score: -4008.542480
+### Recognition: 2nd pass (RL heuristic best-first)
+STAT: 00 _default: 7 generated, 7 pushed, 5 nodes popped in 100
+sentence1: <s> START MUSIC </s>
+wseq1: 0 2 3 1
+phseq1: sil | y ah ng | w ah n | sil
+cmscore1: 1.000 1.000 1.000 1.000
+score1: -11499.305664
+
+#################################
+## __VOCA_FILE__ (perl.voca)
+#################################
+% NS_B
+<s>        sil
+
+% NS_E
+</s>        sil
+
+% CMD
+START      y ah ng
+
+% THING
+MUSIC     w ah n
+TERM      s eh v ax n
+
+######################################
+## __GRAMMAR_FILE__ (perl.grammar)
+######################################
+S : NS_B CMD THING_LOOP NS_E
+THING_LOOP: THING_LOOP THING
+THING_LOOP: THING
