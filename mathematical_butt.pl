0ff0aa93e32fa9884668849b11bd553eb622a555
Trizen <trizenx@gmail.com>
2014-04-29 20:44:28 +0300

    new file:   mathematical_butt.pl

---
+++ mathematical_butt.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+# Author: Trizen
+# License: GPLv3
+# Date: 29 April 2014
+# Website: http://github.com/trizen
+
+use strict;
+use warnings;
+use GD::Simple;
+
+my $img = 'GD::Simple'->new(1000, 1000);
+$img->moveTo(500, 500);
+
+sub t($) {
+    $img->turn(shift);
+}
+
+sub l($) {
+    $img->line(shift);
+}
+
+sub c($) {
+    $img->fgcolor(shift);
+}
+
+for my $i (1 .. 180) {
+    c 'red';
+    for (1 .. 360) {
+        l 4;    # size
+        t 1;
+    }
+    t 0;
+}
+
+my $image_name = 'turtle.png';
+
+open my $fh, '>', $image_name or die $!;
+print {$fh} $img->png;
+close $fh;
+
+system "gliv", $image_name;
