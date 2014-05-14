89af10c760d9710847c09f111223f0bce48bfbb3
Trizen <trizenx@gmail.com>
2014-05-15 02:04:51 +0300

    new file:   File Readers/multi-file-line-reader.pl 	new file:   File Workers/split_to_n_lines.pl

---
+++ File Readers/multi-file-line-reader.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+# Author: Trizen
+# License: GPLv3
+# Date: 13 April 2012
+# http://trizen.googlecode.com
+
+# If you saw this code on perlmonks.org,
+# posted by an Anonymous Monk, that was me.
+
+my (@files) = @ARGV ? @ARGV : ($0, $0);
+
+my @fh;
+my $i = 0;
+
+foreach my $file (@files) {
+    next unless -f -r $file;
+    open $fh[$i++], '<', $file
+      or die "Cannot open ${file}: $!";
+}
+
+while (1) {
+    my @lines;
+
+    foreach my $i (0 .. $#fh) {
+
+        next unless ref $fh[$i] eq 'GLOB';
+        push @lines, scalar readline $fh[$i];
+
+        if (eof $fh[$i]) {
+            close $fh[$i];
+            $fh[$i] = undef;
+        }
+    }
+
+    last unless @lines;
+
+    foreach my $line (@lines) {
+        print $line;
+    }
+}
---
+++ File Workers/split_to_n_lines.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+# Coded by Trizen under the GPL
+# Split a text file into sub files of 'n' lines each other
+# http://trizen.googlecode.com
+
+use strict;
+use warnings;
+
+use Getopt::Std qw(getopts);
+use File::Spec::Functions qw(catfile);
+
+my %opts;
+getopts('l:', \%opts);
+
+my $lines_n = $opts{l} ? int($opts{l}) : 100;
+
+if (not @ARGV) {
+    die "Usage: $0 -l [i] <files>\n";
+}
+
+sub print_to_file {
+    my ($array_ref, $foldername, $num) = @_;
+    open(my $out_fh, '>', catfile($foldername, "$num.txt")) or return;
+    print $out_fh @{$array_ref};
+    close $out_fh;
+    return 1;
+}
+
+foreach my $filename (@ARGV) {
+
+    -f $filename or do {
+        warn "$0: skipping '$filename': is not a file\n";
+        next;
+    };
+
+    my $foldername = $filename;
+    if (not $foldername =~ s/\.\w{1,5}$//) {
+        $foldername .= '_files';
+    }
+
+    if (-d $foldername) {
+        warn "$0: directory '${foldername}' already exists...\n";
+        next;
+    }
+    else {
+        mkdir $foldername or do {
+            warn "$0: Can't create directory '${foldername}': $!\n";
+            next;
+        };
+    }
+
+    open my $fh, '<', $filename or do {
+        warn "$0: Can't open file '${filename}' for read: $!\n";
+        next;
+    };
+
+    my @lines;
+    my $num = 0;
+    while (defined(my $line = <$fh>)) {
+
+        push @lines, $line;
+
+        if (@lines == $lines_n or eof $fh) {
+            print_to_file(\@lines, $foldername, ++$num);
+            undef @lines;
+        }
+    }
+    close $fh;
+}
