ef8fc740e7261141d4903ed6425ae89724c3d288
trizen <trizenx@gmail.com>
2015-04-26 14:29:17 +0300

    new file:   Text/all_substrings.pl

---
+++ Text/all_substrings.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 26 April 2015
+# website: http://github.com/trizen
+
+# Find all the possible substrings of a string. (creative solution)
+
+use 5.012;
+use strict;
+use warnings;
+
+sub all_substrings {
+    my ($str, $callback) = @_;
+
+    my @cache;
+    my @chars = split(//, $str);
+    while (my ($i, $c) = each @chars) {
+        $cache[$_] .= $c for (0 .. $i);
+        $callback->(@cache);
+        $i++;
+    }
+
+    return;
+}
+
+all_substrings("abcdefg", sub { say for @_ });
