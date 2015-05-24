d1ef90212600b1f41f066abe8de63803e3c17112
trizen <trizenx@gmail.com>
2015-05-25 01:50:55 +0300

    new file:   GD/magic_triangle.pl

---
+++ GD/magic_triangle.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 25 May 2015
+# http://github.com/trizen
+
+#
+## Generate a magic triangle using a simple series of numbers
+#
+
+use 5.010;
+use strict;
+use warnings;
+
+use GD::Simple;
+use List::Util qw(max);
+
+my %data;
+
+sub generate {
+    my ($n) = @_;
+
+    my $sum = 0;    # will be incremented by 1, 2, 3, ...
+
+    foreach my $i (1 .. $n) {
+        $sum += $i;
+        $data{$sum} = 1;
+    }
+
+    return 1;
+}
+
+generate(400);
+
+my $i = 1;
+my $j = 1;
+
+my $max   = max(keys %data);
+my $limit = int(sqrt($max)) - 1;
+
+# create a new image
+my $img = GD::Simple->new($limit * 2, $limit + 1);
+
+my $white = 0;
+for my $m (reverse(0 .. $limit)) {
+    $img->moveTo($m, $i - 1);
+
+    for my $n ($j .. $i**2) {
+        if (exists $data{$j}) {
+            $img->fgcolor('blue');
+            $white = 0;
+        }
+        elsif (not $white) {
+            $white = 1;
+            $img->fgcolor('white');
+        }
+        $img->line(1);
+        ++$j;
+    }
+    ++$i;
+}
+
+open my $fh, '>:raw', 'magic_triangle.png';
+print $fh $img->png;
+close $fh;
