4b81846ade731603fc015b3f6906c1dff18f47e2
trizen <trizenx@gmail.com>
2015-05-26 03:34:26 +0300

    new file:   wavy_triangle.pl

---
+++ GD/wavy_triangle.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 26 May 2015
+# http://github.com/trizen
+
+#
+## Generate a wavy triangle using the power of three (scaled down by a trivial constant)
+#
+
+use 5.010;
+use strict;
+use warnings;
+use GD::Simple;
+
+sub generate {
+    my ($n, $data) = @_;
+
+    for my $i (0 .. $n) {
+        $data->{int(($i**3) / 12000)} = 1;
+    }
+
+    return $n;
+}
+
+say "** Generating...";
+
+my %data;
+my $max = generate(500000, \%data);
+my $limit = int(sqrt($max)) - 1;
+
+# create a new image
+my $img = GD::Simple->new($limit * 2, $limit + 1);
+
+my $i = 1;
+my $j = 1;
+
+my $white = 0;
+for my $m (reverse(0 .. $limit)) {
+    $img->moveTo($m, $i - 1);
+
+    for my $n ($j .. $i**2) {
+        if (exists $data{$j}) {
+            $img->fgcolor('blue');
+            $white = 0;
+        }
+        elsif (not $white) {
+            $white = 1;
+            $img->fgcolor('white');
+        }
+        $img->line(1);
+        ++$j;
+    }
+    ++$i;
+}
+
+open my $fh, '>:raw', 'wavy_triangle.png';
+print $fh $img->png;
+close $fh;
