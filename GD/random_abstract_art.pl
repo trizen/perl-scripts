d396b75e9fc42af3f5fd1feb68d6ec9b13c51e73
trizen <trizenx@gmail.com>
2015-06-08 20:55:35 +0300

    new file:   random_abstract_art.pl new file:   random_abstract_art_2.pl

---
+++ GD/random_abstract_art.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 08 June 2015
+# http://github.com/trizen
+
+#
+## Generate complex random art based on simple mathematics.
+#
+
+use 5.010;
+use strict;
+use warnings;
+use GD::Simple;
+use List::Util qw(shuffle);
+
+my $max   = 1_000_000;
+my $limit = int(sqrt($max));
+
+say "Possible combinations: $limit!";
+
+# create a new image
+my $img = GD::Simple->new($limit * 3, $limit * 3);
+
+# move to right
+$img->moveTo($limit *1.5, $limit *1.5);
+
+my $i = 1;
+my $j = 1;
+
+for my $m (shuffle(1 .. $limit)) {
+
+    for my $n ($j .. $i**2) {
+        $img->line(1);
+        $img->turn($n**2 / $m);
+        ++$j;
+    }
+
+    ++$i;
+}
+
+open my $fh, '>:raw', "random_abstract_art.png";
+print $fh $img->png;
+close $fh;
