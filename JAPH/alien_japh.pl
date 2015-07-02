4033917031f1ecffd85cdbd5edd2e49ce0ec98f9
trizen <trizenx@gmail.com>
2015-07-03 01:08:21 +0300

    modified:   alien_japh.pl -- randomized the DATA

--- JAPH/alien_japh.pl
+++ JAPH/alien_japh.pl
@@ -11,40 +11,40 @@
   @x}$/"while@x;$k=$g=chr(ord$^);for(split$\,$s){$s=$g;$$s=$_
   ;++$g}$==135;print$w="\e[H","\e[J";{print$w;print$"x$=,$$_?
  chop$$_:''for$k..$g;select$,,$,,$,,.01;--$=&&redo}}__END__!!!
- ******C*******w*******X*******y*******X*******o*******f******
- igpvAoBLhCffXgIIlyI8gFC8L88vILCg98Io81gaICXpIIg8CIvwFB8I8wXgC
- vIAgLA,L>8CgCCyywcIiF>L=8LX='CgCLfgvC8wXgXKef9B8CIggvIALKXLCv
-y>IgXIXg8w1}CA=y8ylAyw=8Cgyffy8loKK88A8f=,II'gfFFwfvgvCAC8yyLIg
-KXf'''IAX=yiovg>C,8gIAgvAIXFjgCy8Xv89v'XIILy=AC1A8yvov9KvXywffX
-8CFyCC9LvfCvF8gg$yv8vALIIILKsKXyvgCI8yfIKF8L,I9C8BiFwfg,A8h8gF'
-BvgL8C8FfXCC8gB,Iv88AgC8X1CCIFuCX8L>Xi=CCv8ICI8I>KC8IFB8oIFKAvA
-LvgCIg'wBAFLg'1''f=yLLI'ff'fo9gIA>yFv8FIoy'CLfI8f8vk'y8F=vw>gKf
-vy8X        >KLXgKw'og'vF1By'gBvLIXX8KB'XvA'8vofilg        CgC'
-fyBA           8iLIy8IoIvoC,yg,gI=yC8i'I8gL>8'9{           8gB>
-AF18              I8A=vyA'1pfwv,I8lvIABACffIy              AyFC
-1Avpg               Cv'KIyK8C'g9IyFKIL8A=vo               yCABX
-Ffv8A                C,9wyIKI,Kn=iXf8wL1w9                8,ygf
-X88oKC                 ICII8'F8ILCLy>>If                 CC8LCy
- XCAIg                  CFAwBvCfyAIgIyA                  BI9'g
- gyIwL8                  lgXIXXXAX8gI8                  8IBiyX
-  FXAygA                  vgoFFFXAggC                  i,LI>I
-   KIXgt                   vXCA8prCI                   gAK=y
-   ******                  *********                  ******
-    *******                 *******                 *******
-     *******                *******                *******
-      ********               *****               ********
-       *********             *****             *********
-        **********           *****           **********
-         ************       *******       ************
-          *******************************************
-           *****************************************
-            ***************************************
-             *************************************
-               *********************************
-                *******************************
-                 *****************************
-                   *************************
-                     *********************
-                       *****************
-                         *************
-                           *********
+ C**wX*yX****o*f*****ig*pv*AoB**LhCffX*g*I**I*lyI****8*g*FC8L8
+ 8**v*I*LC*g9*8I*o81ga***ICXp*I*Ig8***CIv*wF*B*8*I*8*wX**gCvIA
+ g*LA,L>8*Cg*CCyy*w**cIi**F>*L=8**L*X**='CgCLfg*vC8wXgX*Kef*9*
+B8C**I*g*gvIALKX*L**C*vy*>I*gX***I*Xg8**w1*}CA*=y8*y******lAyw*
+=8C*gy*f****f**y*8**loK****K88A**8f=,*II****'g*f*F*F*wf*v**gvCA
+C8*y*y*LIgK******Xf''***'*I***A*X=yiov***g>C*,*8*g****IAgvA*I*X
+Fj**gCy*8Xv**89v'*XI*ILy**=A**C1A*8y*v*o**v9KvXyw**f**f**X8**C*
+*Fy******C*C9**L*vf**C*vF*8gg$*y**v*8v**AL**II*ILKsK*Xyv**gCI**
+8**y        fI**K*F*8**L*,*I9*C**8*BiFw*fg,A8h8*gF'        B***
+vg*L           *8*C*8*F*fX*CC8*g*B***,Iv88A*****           gC8X
+*1*C              C***IF*u******CX8**L>Xi=***              *C**
+C*v8*               **ICI8I*>*KC***8IF*B*8*               *oIF*
+**K**                Av*ALvg**C*I*g**'*wBA                **FLg
+*'1''*                 f=*yLLI*****'ff'*                 *fo*9g
+ *IA>y                  F***v*8FIoy'*C*                  *Lf**
+ I*8f8v                  *k'y8F*=vw*>g                  **Kf**
+  vy8*X>                  *K*L*XgKw*'                  o*g'vF
+   *1By'                   gBv*LI***                   XX8**
+   KB***'                  Xv**A'**8                  v***of
+    ilgCgC*                 **'***f                 y*BA*8i
+     **LI*y*                *8IoIv*                *oC,y*g
+      ,**gI**=               *yC8i               'I8g*L>8
+       '9{8gB*>A             F*18I             **8A=*v**
+        *yA'*1pf**           *wv**           ****,I8l*v
+         *IAB*AC*f**f       I**yA**       yF*C1*Avp***
+          **gCv'*****KIyK******8C'g9I**yFK*IL8*A=vo*y
+           C*ABX*F*fv8***AC,*9***wy***IKI****,Kn=i**
+            **Xf8w*L1*w*9***8*,ygf*X**88oKCIC***I**
+             I*8*'*F8IL***CL**y*>*>I*****fCC*8LC*y
+               **XC***A*I*g*C*FA*w****Bv**CfyA*I
+                g*I*y*****A*BI9'g**gy*Iw*L8*lgX
+                 *IXXX*A**X**8g*I88*I***B*i***
+                   *y**X*FXAyg****Avgo*F*F*F
+                     *XAg*gC*i****,*LI**>*
+                       IK*I*Xg****tv****
+                         XCA**8pr*CIg*
+                           A*K=***y*
