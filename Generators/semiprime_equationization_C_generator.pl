5b6d53f50e707d7a9d8aa93436a2e3a1c85d4177
trizen <trizenx@gmail.com>
2015-07-10 02:42:08 +0300

    new file:   Generators/semiprime_equationization_C_generator.pl

---
+++ Generators/semiprime_equationization_C_generator.pl
@@ -0,0 +1,116 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 10 July 2015
+# Website: https://github.com/trizen
+
+# Generate a C program to compute the prime factors of a semiprime number.
+
+use 5.016;
+use strict;
+use integer;
+use warnings;
+
+sub semiprime_equationization {
+    my ($semiprime, $xlen, $ylen) = @_;
+
+    $xlen -= 1;
+    $ylen -= 1;
+
+    my @map;
+    my $mem = '0';
+    my @result;
+
+    push @result,
+      map { ("int x$_;", $_ == $xlen ? "for (x$_ = 1; x$_ <= 9; x$_++) {" : "for (x$_ = 0; x$_ <= 9; x$_++) {") } 0 .. $xlen;
+    push @result,
+      map { ("int y$_;", $_ == $ylen ? "for (y$_ = 1; y$_ <= 9; y$_++) {" : "for (y$_ = 0; y$_ <= 9; y$_++) {") } 0 .. $ylen;
+
+    my %vars;
+    foreach my $j (0 .. $ylen) {
+        foreach my $i (0 .. $xlen) {
+            my $expr = '(' . join(' + ', "(x$i * y$j)", grep { $_ ne '0' } $mem) . ')';
+
+            $vars{"xy$i$j"} = $expr;
+            my $n = "xy$i$j";
+
+            if ($i == $xlen) {
+                push @{$map[$j]}, "($n % 10)", "($n / 10)";
+                $mem = '0';
+            }
+            else {
+                push @{$map[$j]}, "($n % 10)";
+                $mem = "($n / 10)";
+            }
+        }
+
+        my $n = $ylen - $j;
+        if ($n > 0) {
+            push @{$map[$j]}, ((0) x $n);
+        }
+
+        my $m = $ylen - $n;
+        if ($m > 0) {
+            unshift @{$map[$j]}, ((0) x $m);
+        }
+    }
+
+    my @number = reverse split //, $semiprime;
+
+    my @mrange = (0 .. $#map);
+    my $end    = $xlen + $ylen + 1;
+
+    my %seen;
+    my $initializer = sub {
+        my ($str) = @_;
+
+        while ($str =~ /\b(xy\d+)/g) {
+            if (not $seen{$1}++) {
+                my $init = "int $1 = $vars{$1};";
+                __SUB__->($init);
+                push @result, $init;
+            }
+        }
+    };
+
+    foreach my $i (0 .. $#number) {
+        my $expr = '(' . join(' + ', grep { $_ ne '0' } (map { $map[$_][$i] } @mrange), $mem) . ')';
+        $initializer->($expr);
+
+        push @result, "int n$i = $expr;";
+        my $n = "n$i";
+
+        if ($i == $#number) {
+            push @result,
+                qq/if ($number[$i] == ($n % 10)) { printf("Cracked: /
+              . ("%d" x ($xlen + 1))
+              . (" * ")
+              . ("%d" x ($ylen + 1))
+              . qq/\\n", /
+              . join(", ", (map { "x$_" } reverse(0 .. $xlen)), (map { "y$_" } reverse(0 .. $ylen)))
+              . qq/); return 0; }/;
+        }
+        else {
+            push @result, "if ($number[$i] != ($n % 10)) { continue; }";
+            $mem = "($n / 10)";
+        }
+    }
+
+    unshift @result, "#include <stdio.h>", "int main() {";
+    push @result, "}" x (1 + $xlen + 1 + $ylen + 1);
+
+    return @result;
+}
+
+# 71 * 43
+#say for semiprime_equationization('3053', 2, 2);
+
+# 251 * 197
+#say for semiprime_equationization('49447', 3, 3);
+
+# 7907 * 4999
+say for semiprime_equationization('39527093', 4, 4);
+
+# 37975227936943673922808872755445627854565536638199 * 40094690950920881030683735292761468389214899724061
+#say for semiprime_equationization('1522605027922533360535618378132637429718068114961380688657908494580122963258952897654000350692006139', 50, 50);
