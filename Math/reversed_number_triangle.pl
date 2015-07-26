a420f2680f47e6ce06e73c062acdc6652c0ea445
trizen <trizenx@gmail.com>
2015-07-26 17:05:31 +0300

    new file

---
+++ Math/reversed_number_triangle.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 26 July 2015
+# Website: https://github.com/trizen
+
+# Generate a "reversed" number triangle.
+
+my $rows = 6;
+my @arr  = ([1]);
+
+my $n = 1;
+foreach my $i (0 .. $rows) {
+
+    foreach my $j (reverse 0 .. $#arr) {
+        push @{$arr[$j]}, ++$n;
+        unshift @{$arr[$j]}, ++$n;
+    }
+
+    unshift @arr, [++$n];
+}
+
+foreach my $row (@arr) {
+    print " " x (3 * (1 + $rows--));
+    print map { sprintf "%3d", $_ } @{$row};
+    print "\n";
+}
