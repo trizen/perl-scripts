4a8d5ad198db59b09f4ab6e83271c32e2e3ae4f3
trizen <trizenx@gmail.com>
2015-09-04 00:03:33 +0300

    modified:   faulhaber_s_formulas_generator.pl

--- Generators/faulhaber_s_formulas_generator.pl
+++ Generators/faulhaber_s_formulas_generator.pl
@@ -5,7 +5,7 @@
 # Date: 03 September 2015
 # Website: https://github.com/trizen
 
-# The scrips generates formulas for calculating the sum of series such as:
+# The script generates formulas for calculating the sum of series such as:
 #    1^p + 2^p + 3^p + ... n^p
 # where p is a positive integer.
 
@@ -65,3 +65,16 @@ sub faulhaber_s_formula {
 foreach my $i (0 .. 10) {
     say "$i: ", faulhaber_s_formula($i);
 }
+
+__END__
+0: 1/1 * (n)
+1: 1/2 * (n^2 + n)
+2: 1/3 * (n^3 + (3/2)*n^2 + (1/2)*n)
+3: 1/4 * (n^4 + (2)*n^3 + n^2)
+4: 1/5 * (n^5 + (5/2)*n^4 + (5/3)*n^3 + (-1/6)*n)
+5: 1/6 * (n^6 + (3)*n^5 + (5/2)*n^4 + (-1/2)*n^2)
+6: 1/7 * (n^7 + (7/2)*n^6 + (7/2)*n^5 + (-7/6)*n^3 + (1/6)*n)
+7: 1/8 * (n^8 + (4)*n^7 + (14/3)*n^6 + (-7/3)*n^4 + (2/3)*n^2)
+8: 1/9 * (n^9 + (9/2)*n^8 + (6)*n^7 + (-21/5)*n^5 + (2)*n^3 + (-3/10)*n)
+9: 1/10 * (n^10 + (5)*n^9 + (15/2)*n^8 + (-7)*n^6 + (5)*n^4 + (-3/2)*n^2)
+10: 1/11 * (n^11 + (11/2)*n^10 + (55/6)*n^9 + (-11)*n^7 + (11)*n^5 + (-11/2)*n^3 + (5/6)*n)
