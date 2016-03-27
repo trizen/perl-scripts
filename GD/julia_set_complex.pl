562aa1a8883cd7ffe3651670d1a9d1336e86e128
trizen <trizenx@gmail.com>
2016-03-27 05:44:30 +0300

    new file:   GD/julia_set_complex.pl

---
+++ GD/julia_set_complex.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 27 March 2016
+# Website: https://github.com/trizen
+
+# Generate random fractals contained in the Julia set.
+# Formula: f(z) = z^2 + c
+
+# See also: https://en.wikipedia.org/wiki/Julia_set
+#           http://rosettacode.org/wiki/Julia_set
+
+use strict;
+use warnings;
+
+use Imager;
+use Inline 'C';
+
+for (1 .. 100) {
+
+    my ($w, $h) = (800, 600);
+    my $img = Imager->new(xsize => $w, ysize => $h, channels => 3);
+
+    ##my $maxIter = int(rand(200))+50;
+    my $maxIter = 50;
+
+    ##my ($cx, $cy) = (-rand(1), rand(1));
+    ##my ($cx, $cy) = (1-rand(2), 1-rand(2));         # cool
+    my ($cx, $cy) = (1 - rand(2), rand(1));    # nice
+    ##my ($cx, $cy) = (1 - rand(2), 2 - rand(3));
+    ##my ($cx, $cy) = ((-1)**((1,2)[rand(2)]) * rand(2), (-1)**((1,2)[rand(2)]) * rand(2));
+
+    my $color = Imager::Color->new('#000000');
+
+    foreach my $x (0 .. $w - 1) {
+        foreach my $y (0 .. $h - 1) {
+            my $i = iterate(($x - $w / 2) / $w * 3, ($y - $h / 2) / $h * 2, $cx, $cy, $maxIter);
+            $color->set(hsv => [$i / $maxIter * 360 - 120, 1, $i]);
+            $img->setpixel(x => $x, y => $y, color => $color);
+        }
+    }
+
+    print "Writing new image...\n";
+    $img->write(file => "i=$maxIter;c=$cx+$cy.png");
+}
+
+__END__
+__C__
+
+#include <complex.h>
+
+int iterate(double zx, double zy, double cx, double cy, int i) {
+    double complex z = zx + zy * I;
+    double complex c = cx + cy * I;
+    while (cabs(z) < 4 && --i) {
+        z = z*z + c;
+        //z = z * cexp(z) + c;
+        //z = ccosh(z) + c;
+        //z = z * csinh(z) + c;
+        //z = z * ccosh(z) + c;
+        //z = clog(csinh(z)) + c;
+        //z = csqrt(cexp(z) + ccosh(z)) + c;
+    }
+    return i;
+}
