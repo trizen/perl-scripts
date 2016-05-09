644090aeda50f2784a70a9d6c7bad05a8942b0a2
trizen <trizenx@gmail.com>
2016-05-10 00:49:55 +0300

    new file:   Math/is_sum_of_two_squares.pl

---
+++ Math/is_sum_of_two_squares.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 10 May 2016
+# Website: https://github.com/trizen
+
+# Determine if a given number can be written as the sum of two squares.
+# See also: http://wstein.org/edu/Fall2001/124/lectures/lecture21/lecture21/node2.html
+
+use 5.010;
+use strict;
+use warnings;
+
+use ntheory qw(factor is_prime);
+
+sub is_sum_of_2_squares {
+    my ($n) = @_;
+
+    if (is_prime($n)) {
+        return 1 if $n == 2;
+        return $n % 4 == 1;
+    }
+
+    my %map;
+    foreach my $p (grep { $_ % 4 == 3 } factor($n)) {
+        $map{$p}++;
+    }
+
+    foreach my $key (keys %map) {
+        $map{$key} % 2 == 0 or return;
+    }
+
+    return 1;
+}
+
+for my $i (0 .. 50) {
+    if (is_sum_of_2_squares($i)) {
+        say $i;
+    }
+}
