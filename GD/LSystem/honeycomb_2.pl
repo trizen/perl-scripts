f72a1f60e02e0db1e5cb44d49c26771f68565c2c
trizen <trizenx@gmail.com>
2016-05-09 18:31:06 +0300

    modified:   GD/LSystem/honeycomb_2.pl

--- GD/LSystem/honeycomb_2.pl
+++ GD/LSystem/honeycomb_2.pl
@@ -6,18 +6,21 @@ use warnings;
 
 use LSystem;
 
-my %rules = (F => 'F[-F][+F]');
+my %rules = (
+             F => '+F-F-F-F-F-F-F-F-F+',    # or: '+F-F-F-F-F-F-F+'
+            );
 
 my $lsys = LSystem->new(
-    width  => 1000,
+    width  => 1200,
     height => 1000,
 
     scale => 1,
-    xoff  => -500,
-    yoff  => -500,
+    xoff  => -600,
+    yoff  => -180,
 
     len   => 20,
     angle => 60,
     color => 'orange',
                        );
-$lsys->execute('F', 10, "honeycomb_2.png", %rules);
+
+$lsys->execute('F', 5, "honeycomb_2.png", %rules);
