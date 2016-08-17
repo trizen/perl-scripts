5369311a598a68d5d5811b06566e21e79bd250c1
trizen <trizenx@gmail.com>
2016-08-17 16:17:04 +0300

    new file:   Math/area_of_triangle.pl

---
+++ Math/area_of_triangle.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# Author: Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 17 August 2016
+# Website: https://github.com/trizen
+
+# Find the area of a triangle all three sides of a triangle are known, using Heron's Formula.
+
+use 5.010;
+use strict;
+use warnings;
+
+sub triangle_area {
+    my ($x, $y, $z) = @_;
+    my $s = ($x + $y + $z) / 2;
+    sqrt($s * ($s - $x) * ($s - $y) * ($s - $z));
+}
+
+say triangle_area(5, 5, 6);
