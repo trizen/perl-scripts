f416649c41194539e84b4fabce7e38abf6a52dbb
trizen <trizenx@gmail.com>
2016-12-24 04:53:54 +0200

    modified:   Game solvers/reflex_sheep_game.pl

--- Game solvers/reflex_sheep_game.pl
+++ Game solvers/reflex_sheep_game.pl
@@ -18,22 +18,21 @@ my $count = 0;
 
 ROOT: while (1) {
 
-    my $gd = GD::Image->new(scalar `maim -x 640 -y 150 -w 10 -h 850 --format=jpg /dev/stdout`);
+    my $gd = GD::Image->new(scalar `maim -x 640 -y 150 -w 1 -h 850 --format=jpg /dev/stdout`);
+
     #my $gd = GD::Image->new(scalar `maim -x 555 -y 100 -w 10 -h 650 --format=jpg /dev/stdout`);      # faster, but buggy
 
     my ($width, $height) = $gd->getBounds;
 
   OUTER: foreach my $y (0 .. $height - 1) {
-        foreach my $x (0 .. $width - 1) {
-            my $pixel = $gd->getPixel(0, $y);
-            my ($r, $g, $b) = $gd->rgb($pixel);
-            my $avg = ($r + $g + $b) / 3;
-            if ($avg < 50) {
-                sleep(0.07);    # let the ship to run a little bit more
-                system("xdotool", "click", "1");
-                sleep(1);       # sleep a little bit after clicking
-                ++$count == 5 ? last ROOT: last OUTER;
-            }
+        my $pixel = $gd->getPixel(0, $y);
+        my ($r, $g, $b) = $gd->rgb($pixel);
+        my $avg = ($r + $g + $b) / 3;
+        if ($avg < 50) {
+            sleep(0.085);    # let the ship run a little bit more
+            system("xdotool", "click", "1");
+            sleep(1);        # sleep a little bit after the click
+            ++$count == 5 ? last ROOT: last OUTER;
         }
     }
 }
