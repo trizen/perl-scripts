0c2a54f718b20fb6f214b3349fda45d28f93a11e
trizen <trizenx@gmail.com>
2016-12-27 04:40:22 +0200

    new file:   Math/lambert_W_function_complex.pl modified:   Math/logarithmic_root_complex.pl

--- Math/lambert_W_function_complex.pl
+++ Math/lambert_W_function_complex.pl
@@ -2,18 +2,17 @@
 
 # Daniel "Trizen" Șuteu
 # License: GPLv3
-# Date: 17 September 2016
-# Website: https://github.com/trizen
+# Date: 26 December 2016
+# https://github.com/trizen
 
-# Logarithmic root of n.
-# Solves c = x^x, where "c" is known.
-# (based on Newton's method for nth-root)
+# Implementation of the Lambert-W function in complex numbers.
 
-# Example: 100 = x^x
-#          x = lgrt(100)
-#          x =~ 3.59728502354042
+# Example: x^x = 100
+#            x = exp(lambert_w(log(100)))
+#            x =~ 3.59728502354042
 
-# The function is defined in complex numbers for any value != 0.
+# See also:
+#   https://en.wikipedia.org/wiki/Lambert_W_function
 
 use 5.010;
 use strict;
@@ -25,7 +24,7 @@ use Math::MPFR;
 my $PREC  = 128;                      # can be tweaked
 my $ROUND = Math::MPC::MPC_RNDNN();
 
-sub lgrt {
+sub lambert_w {
     my ($c) = @_;
 
     if (ref($c) ne 'Math::MPC') {
@@ -38,14 +37,10 @@ sub lgrt {
     Math::MPFR::Rmpfr_ui_pow_ui($p, 10, int($PREC / 4), $ROUND);
     Math::MPFR::Rmpfr_ui_div($p, 1, $p, $ROUND);
 
-    my $d = Math::MPC::Rmpc_init2($PREC);
-    Math::MPC::Rmpc_log($d, $c, $ROUND);
-
     my $x = Math::MPC::Rmpc_init2($PREC);
     Math::MPC::Rmpc_set($x, $c, $ROUND);
-    Math::MPC::Rmpc_sqr($x, $x, $ROUND);
+    Math::MPC::Rmpc_sqrt($x, $x, $ROUND);
     Math::MPC::Rmpc_add_ui($x, $x, 1, $ROUND);
-    Math::MPC::Rmpc_log($x, $x, $ROUND);
 
     my $y = Math::MPC::Rmpc_init2($PREC);
     Math::MPC::Rmpc_set_ui($y, 0, $ROUND);
@@ -56,6 +51,7 @@ sub lgrt {
     my $count = 0;
     while (1) {
         Math::MPC::Rmpc_sub($tmp, $x, $y, $ROUND);
+
         Math::MPC::Rmpc_abs($abs, $tmp, $ROUND);
         Math::MPFR::Rmpfr_cmp($abs, $p) <= 0 and last;
 
@@ -64,14 +60,15 @@ sub lgrt {
         Math::MPC::Rmpc_log($tmp, $x, $ROUND);
         Math::MPC::Rmpc_add_ui($tmp, $tmp, 1, $ROUND);
 
-        Math::MPC::Rmpc_add($x, $x, $d, $ROUND);
+        Math::MPC::Rmpc_add($x, $x, $c, $ROUND);
         Math::MPC::Rmpc_div($x, $x, $tmp, $ROUND);
         last if ++$count > $PREC;
     }
 
+    Math::MPC::Rmpc_log($x, $x, $ROUND);
     $x;
 }
 
-say lgrt(100);     # 3.597285023540417505497652251782286069146
-say lgrt(-100);    # 3.702029366602145942901939629527371028025 + 1.348231284711519013278314649698724804171i
-say lgrt(-1);      # 1.690386757163589211290419139332364873691 + 1.869907964026775775222799239924290781916i
+say lambert_w(100);     #  3.385630140290050184888244364529726867493
+say lambert_w(-100);    #  3.205380786307449372155918213968303847481  + 2.482590531815923582117041287234452276982i
+say lambert_w(-0.5);    # -0.7940236323446893679630153219005898091005 + 0.770111750510379109681313077405028929402i
