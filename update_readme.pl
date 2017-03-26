de469d4bc8cc08912b7c77955d98ef02efbd72d9
trizen <trizenx@gmail.com>
2017-03-26 19:18:21 +0300

    - Escape only spaces in README.md

--- update_readme.pl
+++ update_readme.pl
@@ -74,7 +74,7 @@ my $main_dir     = File::Spec->curdir;
             else {
                 next if $dir eq $main_dir;
                 my $naked_title = $title =~ s/\.pl\z//ri;
-                my $url_path    =uri_escape($make_section_url->($file->{name}));
+                my $url_path    =uri_escape($make_section_url->($file->{name}), ' ');
                 $section .= (' ' x $spaces) . "* [\u$naked_title]($url_path)\n";
             }
         }
