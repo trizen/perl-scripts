de9636ac73ef28f30ecd11fc8395775cd5288102
trizen <trizenx@gmail.com>
2017-03-26 19:01:08 +0300

    - Escape link paths in README.md

--- update_readme.pl
+++ update_readme.pl
@@ -15,6 +15,7 @@ use warnings;
 use Cwd qw(getcwd);
 use File::Spec qw();
 use File::Basename qw(basename dirname);
+use URI::Escape qw(uri_escape);
 
 sub add_section {
     my ($section, $file) = @_;
@@ -73,7 +74,7 @@ my $main_dir     = File::Spec->curdir;
             else {
                 next if $dir eq $main_dir;
                 my $naked_title = $title =~ s/\.pl\z//ri;
-                my $url_path    = $make_section_url->($file->{name});
+                my $url_path    =uri_escape($make_section_url->($file->{name}));
                 $section .= (' ' x $spaces) . "* [\u$naked_title]($url_path)\n";
             }
         }
