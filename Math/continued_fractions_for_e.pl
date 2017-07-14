69a895e8d8bb072f6aaffac30bae5f47a676d8c5
Daniel Șuteu <trizenx@gmail.com>
2017-07-14 07:24:49 +0300

    Updated Math/continued_fractions_for_e.pl

--- Math/continued_fractions_for_e.pl
+++ Math/continued_fractions_for_e.pl
@@ -25,6 +25,48 @@ sub e_3 {
     $limit > 0 ? (1 / (2 * $i + 1 + e_3($i + 1, $limit - 1))) : 0;
 }
 
-say 1 + 1 / e_1(1, 100);          # very fast convergence
-say 2 + e_2(1, 100);              # extremely fast convergence
-say sqrt(1 + 2 / e_3(1, 100));    # extremely fast convergence
+sub e_4 {
+    my ($i, $n) = @_;
+
+    return 0 if $n >= $i;
+
+    1 / (
+        1 + 1 / (
+            1 + 1 / (
+                (3 * $n) + 1 / (
+                    (12 * $n + 6) + 1 / (
+                        (3 * $n + 2) + e_4($i, $n + 1)
+                    )
+                )
+            )
+        )
+    );
+}
+
+sub e_5 {
+    my ($i, $n) = @_;
+
+    return 0 if $n >= $i;
+
+    1 / (
+        3 + 1 / (
+            2*$n + 1 / (
+                3 + 1 / (
+                    1 + 1 / (
+                        2*$n + 1 / (
+                            1 + e_5($i, $n + 1)
+                        )
+                    )
+                )
+            )
+        )
+    );
+}
+
+my $r = 100;        # number of repetitions
+
+say 1 + 1 / e_1(1, $r);                  # very fast convergence
+say 2 + e_2(1, $r);                      # extremely fast convergence
+say sqrt(1 + 2 / e_3(1, $r));            # extremely fast convergence
+say sqrt(7 + 1 / (2 + (e_4($r, 1))));    # ultra-fast convergence
+say ((5 + 1/(2 +  e_5($r, 1)))/2);       # ultra-fast convergence
