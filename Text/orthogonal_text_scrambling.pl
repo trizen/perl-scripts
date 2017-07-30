e5cbf222abc17bd08f94e2b8b473c9e1378c9efd
Daniel Șuteu <trizenx@gmail.com>
2017-07-30 10:26:51 +0300

    Added two new Text scripts.

---
+++ Text/orthogonal_text_scrambling.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 29 July 2017
+# https://github.com/trizen
+
+# An interesting text scrambling algorithm, invented by the author in ~2008.
+
+use 5.010;
+use strict;
+use warnings;
+
+sub scramble {
+    my ($str) = @_;
+
+    my $i = length($str);
+    $str =~ s/(.{$i})(.)/$2$1/g while --$i > 0;
+    return $str;
+}
+
+sub unscramble {
+    my ($str) = @_;
+
+    my $i = 0;
+    my $l = length($str);
+    $str =~ s/(.)(.{$i})/$2$1/g while (++$i < $l);
+    return $str;
+}
+
+my $abc = "abcdefghijklmnopqrstuvwxyz";
+
+say scramble($abc);                #=> "fvjnabdsgrpzxqeholmictyuwk"
+say unscramble(scramble($abc));    #=> "abcdefghijklmnopqrstuvwxyz"
---
+++ Text/orthogonal_text_scrambling_double.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# License: GPLv3
+# Date: 29 July 2017
+# https://github.com/trizen
+
+# An interesting text scrambling algorithm, invented by the author in ~2008.
+
+use 5.010;
+use strict;
+use warnings;
+
+sub scramble {
+    my ($str) = @_;
+
+    my $i = my $l = length($str);
+
+    $str =~ s/(.{$i})(.)/$2$1/g while --$i > 0;
+    $str =~ s/(.{$i})(.)/$2$1/g while ++$i < $l;
+
+    return $str;
+}
+
+sub unscramble {
+    my ($str) = @_;
+
+    my $i = my $l = length($str);
+
+    $str =~ s/(.)(.{$i})/$2$1/g while --$i > 0;
+    $str =~ s/(.)(.{$i})/$2$1/g while ++$i < $l;
+
+    return $str;
+}
+
+my $abc = "abcdefghijklmnopqrstuvwxyz";
+
+say scramble($abc);                #=> "ckytmliqzrbjwuexhogpdsanvf"
+say unscramble(scramble($abc));    #=> "abcdefghijklmnopqrstuvwxyz"
