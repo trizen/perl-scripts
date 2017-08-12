301d6e893d7cef18ddddf04b9c7f9e1f61e5d90d
Daniel Șuteu <trizenx@gmail.com>
2017-08-12 19:00:55 +0300

    Updated two Math scripts.

--- Math/difference_of_two_squares_solutions.pl
+++ Math/difference_of_two_squares_solutions.pl
@@ -2,15 +2,16 @@
 
 # Daniel "Trizen" Șuteu
 # Date: 09 August 2017
+# Edit: 12 August 2017
 # https://github.com/trizen
 
-# An algorithm for finding solutions to an equation:
+# An efficient algorithm for finding solutions to the equation:
 #
-#   x^2 - y^2 = 4*n
+#   x^2 - y^2 = n
 #
 # where only `n` is known.
 
-# This algorithm uses the divisors of `n` to generate all the non-negative integer solutions.
+# This algorithm uses the divisors of `n` to generate all the positive integer solutions.
 
 use 5.010;
 use strict;
@@ -21,30 +22,29 @@ use ntheory qw(divisors sqrtint);
 sub difference_of_two_squares_solutions {
     my ($n) = @_;
 
-    if ($n % 4 != 0) {    # must be a multiple of 4
-        return;
-    }
-
-    $n >>= 2;
-
     my @divisors = divisors($n);
 
     my @solutions;
     foreach my $divisor (@divisors) {
 
-        last if $divisor > sqrt($n);
+        last if $divisor >= sqrt($n);
 
         my $p = $divisor;
         my $q = $n / $divisor;
-        my $d = abs($p - $q);
+        my $d = $q - $p;
 
-        unshift @solutions, [sqrtint($d**2 + 4 * $n), $d];
+        $d % 2 == 0 or next;
+
+        my $x = sqrtint($d**2 + 4 * $n) >> 1;
+        my $y = $d >> 1;
+
+        unshift @solutions, [$x, $y];
     }
 
     return @solutions;
 }
 
-my $n         = 1275344;
+my $n         = 12345;
 my @solutions = difference_of_two_squares_solutions($n);
 
 foreach my $solution (@solutions) {
@@ -52,15 +52,7 @@ foreach my $solution (@solutions) {
 }
 
 __END__
-1185^2 - 359^2 = 1275344
-1212^2 - 440^2 = 1275344
-1587^2 - 1115^2 = 1275344
-1845^2 - 1459^2 = 1275344
-2820^2 - 2584^2 = 1275344
-5463^2 - 5345^2 = 1275344
-11415^2 - 11359^2 = 1275344
-22788^2 - 22760^2 = 1275344
-45555^2 - 45541^2 = 1275344
-79713^2 - 79705^2 = 1275344
-159420^2 - 159416^2 = 1275344
-318837^2 - 318835^2 = 1275344
+419^2 - 404^2 = 12345
+1237^2 - 1232^2 = 12345
+2059^2 - 2056^2 = 12345
+6173^2 - 6172^2 = 12345
