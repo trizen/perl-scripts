374393a21d6a6e06ecc0a9017e4caca1664609f4
trizen <trizenx@gmail.com>
2017-09-29 20:34:23 +0300

    new file:   Math/modular_binomial_fast.pl

---
+++ Math/modular_binomial_fast.pl
@@ -0,0 +1,122 @@
+#!/usr/bin/perl
+
+# Efficient algorithm for computing `binomial(n, k) mod m`, based on the factorization of `m`.
+
+# Algorithm by Andrew Granville:
+#     https://www.scribd.com/document/344759427/BinCoeff-pdf
+
+# Algorithm translated from:
+#   https://github.com/hellman/libnum/blob/master/libnum/modular.py
+
+# Translated by: Daniel "Trizen" Șuteu
+# Date: 29 September 2017
+# https://github.com/trizen
+
+use 5.010;
+use strict;
+use warnings;
+
+use experimental qw(signatures);
+use ntheory qw(factor_exp chinese invmod mulmod);
+
+sub modular_binomial ($n, $k, $m) {
+
+    my @rems_mods;
+    foreach my $pair (factor_exp($m)) {
+        my ($p, $e) = @$pair;
+        push @rems_mods, [modular_binomial_prime_power($n, $k, $p, $e), $p**$e];
+    }
+
+    return chinese(@rems_mods);
+}
+
+sub factorial_prime_pow ($n, $p) {
+
+    my $s = 0;
+    while ($n >= $p) {
+        $s += int($n /= $p);
+    }
+
+    return $s;
+}
+
+sub binomial_prime_pow ($n, $k, $p) {
+#<<<
+      factorial_prime_pow($n,      $p)
+    - factorial_prime_pow($k,      $p)
+    - factorial_prime_pow($n - $k, $p);
+#>>>
+}
+
+sub binomial_non_prime_part ($n, $k, $p, $e) {
+    my $pe = $p**$e;
+    my $r  = $n - $k;
+
+    my $acc     = 1;
+    my @fact_pe = (1);
+
+    foreach my $x (1 .. $pe - 1) {
+        if ($x % $p == 0) {
+            $x = 1;
+        }
+        $acc = ($acc * $x) % $pe;
+        push @fact_pe, $acc;
+    }
+
+    my $top = my $bottom = 1;
+    my $is_negative = 0;
+    my $digits      = 0;
+
+    while ($n) {
+
+        if ($acc != 1) {
+            if ($digits >= $e) {
+                $is_negative ^= $n & 1;
+                $is_negative ^= $r & 1;
+                $is_negative ^= $k & 1;
+            }
+        }
+
+#<<<
+        $top    = mulmod($top,    $fact_pe[$n % $pe], $pe);
+        $bottom = mulmod($bottom, $fact_pe[$r % $pe], $pe);
+        $bottom = mulmod($bottom, $fact_pe[$k % $pe], $pe);
+#>>>
+
+        $n = int($n / $p);
+        $r = int($r / $p);
+        $k = int($k / $p);
+
+        ++$digits;
+    }
+
+    my $res = ($top * invmod($bottom, $pe)) % $pe;
+
+    if ($p != 2 or $e < 3) {
+        if ($is_negative) {
+            $res = $pe - $res;
+        }
+    }
+
+    return $res;
+}
+
+sub modular_binomial_prime_power ($n, $k, $p, $e) {
+    my $pow = binomial_prime_pow($n, $k, $p);
+
+    if ($pow >= $e) {
+        return 0;
+    }
+
+    my $modpow = $e - $pow;
+    my $r = binomial_non_prime_part($n, $k, $p, $modpow) % $p**$modpow;
+    return mulmod($p**$pow, $r, $p**$e);
+}
+
+say modular_binomial(12,   5,   100000);     #=> 792
+say modular_binomial(16,   4,   100000);     #=> 1820
+say modular_binomial(100,  50,  139);        #=> 71
+say modular_binomial(1000, 10,  1243);       #=> 848
+say modular_binomial(124,  42,  1234567);    #=> 395154
+say modular_binomial(1e9,  1e4, 1234567);    #=> 833120
+say modular_binomial(1e10, 1e5, 1234567);    #=> 589372
