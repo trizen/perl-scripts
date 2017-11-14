6e8d1bc2ae5df1f7e9b0acf3a4b2a0e1c7661251
trizen <trizenx@gmail.com>
2017-11-14 21:29:42 +0200

    modified:   Math/bernoulli_numbers_from_primes_gmpf.pl - simplifications

--- Math/bernoulli_numbers_from_primes_gmpf.pl
+++ Math/bernoulli_numbers_from_primes_gmpf.pl
@@ -38,16 +38,16 @@ sub bern_from_primes {
     my $log2B = (CORE::log(4 * $tau * $n) / 2 + $n * (CORE::log($n / $tau) - 1)) / CORE::log(2);
 
     my $prec = CORE::int($n + $log2B) +
-          ($n <= 90 ? (3, 3, 4, 4, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 10, 12, 9, 7, 6, 0, 0, 0,
+          ($n <= 90 ? (3, 3, 4, 4, 7, 6, 6, 6, 7, 7, 7, 8, 8, 9, 10, 12, 9, 7, 6, 0, 0, 0,
                        0, 0, 0, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4)[($n>>1)-1] : 0);
 
     state $d = Math::GMPz::Rmpz_init_nobless();
-    Math::GMPz::Rmpz_fac_ui($d, $n);    # d = n!
+    Math::GMPz::Rmpz_fac_ui($d, $n);                      # d = n!
 
     my $K = Math::MPFR::Rmpfr_init2($prec);
-    Math::MPFR::Rmpfr_const_pi($K, $round);    # K = pi
-    Math::MPFR::Rmpfr_pow_si($K, $K, -$n, $round);    # K = K^(-n)
-    Math::MPFR::Rmpfr_mul_z($K, $K, $d, $round);      # K = K*d
+    Math::MPFR::Rmpfr_const_pi($K, $round);               # K = pi
+    Math::MPFR::Rmpfr_pow_si($K, $K, -$n, $round);        # K = K^(-n)
+    Math::MPFR::Rmpfr_mul_z($K, $K, $d, $round);          # K = K*d
     Math::MPFR::Rmpfr_div_2ui($K, $K, $n - 1, $round);    # K = K / 2^(n-1)
 
     # `d` is the denominator of bernoulli(n)
@@ -85,18 +85,18 @@ sub bern_from_primes {
     }
 
     state $N = Math::MPFR::Rmpfr_init2_nobless(64);
-    Math::MPFR::Rmpfr_mul_z($K, $K, $d, $round);    # K = K*d
-    Math::MPFR::Rmpfr_root($N, $K, $n - 1, $round); # N = N^(1/(n-1))
-    Math::MPFR::Rmpfr_ceil($N, $N);                 # N = ceil(N)
+    Math::MPFR::Rmpfr_mul_z($K, $K, $d, $round);         # K = K*d
+    Math::MPFR::Rmpfr_root($N, $K, $n - 1, $round);      # N = N^(1/(n-1))
+    Math::MPFR::Rmpfr_ceil($N, $N);                      # N = ceil(N)
 
     my $bound = Math::MPFR::Rmpfr_get_ui($N, $round);    # bound = int(N)
 
     my $t = Math::GMPf::Rmpf_init2($prec);               # temporary variable
     my $f = Math::GMPf::Rmpf_init2($prec);               # approximation to zeta(n)
 
-    Math::GMPf::Rmpf_set_ui($f, 1);
+    Math::MPFR::Rmpfr_get_f($f, $K, $round);
 
-    for (my $i = 0 ; $primes[$i] <= $bound ; ++$i) {     # primes <= N
+    for (my $i = 0 ; $primes[$i] <= $bound ; ++$i) {  # primes <= N
         Math::GMPf::Rmpf_set_ui($t, $primes[$i]);        # t = p
         Math::GMPf::Rmpf_pow_ui($t, $t, $n);             # t = t^n
         Math::GMPf::Rmpf_mul($f, $f, $t);                # f = f*t
@@ -104,18 +104,13 @@ sub bern_from_primes {
         Math::GMPf::Rmpf_div($f, $f, $t);                # f = f/t
     }
 
-    my $fr = Math::MPFR::Rmpfr_init2($prec);
-    Math::MPFR::Rmpfr_set_f($fr, $f, $round);            # fr = f
-
-    Math::MPFR::Rmpfr_mul($fr, $fr, $K, $round);         # fr = fr*K
-    Math::MPFR::Rmpfr_ceil($fr, $fr);                    # fr = ceil(z)
-
     my $q = Math::GMPq::Rmpq_init();
 
+    Math::GMPf::Rmpf_ceil($f, $f);                       # f = ceil(f)
+    Math::GMPq::Rmpq_set_f($q, $f);                      # q = f
+
     Math::GMPq::Rmpq_set_den($q, $d);                    # denominator
-    Math::MPFR::Rmpfr_get_z($d, $fr, $round);
-    Math::GMPz::Rmpz_neg($d, $d) if $n % 4 == 0;         # d = -d, iff 4|n
-    Math::GMPq::Rmpq_set_num($q, $d);                    # numerator
+    Math::GMPq::Rmpq_neg($q, $q) if $n % 4 == 0;         # q = -q, iff 4|n
 
     return $q;                                           # Bn
 }
