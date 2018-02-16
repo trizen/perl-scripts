1227b273ab2792d9a0fd8ca0239e8611ec2e53ae
trizen <trizen@protonmail.com>
2018-02-16 02:05:09 +0200

    modified:   Math/pollard_p_minus_1_factorization.pl

--- Math/pollard_p_minus_1_factorization.pl
+++ Math/pollard_p_minus_1_factorization.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Simple implementation of Pollard's p−1 integer factorization algorithm. (unoptimized)
+# Simple implementation of Pollard's p−1 integer factorization algorithm.
 
 # See also:
 #   https://en.wikipedia.org/wiki/Pollard%27s_p_%E2%88%92_1_algorithm
@@ -9,7 +9,8 @@ use 5.010;
 use strict;
 use warnings;
 
-use Math::AnyNum qw(:overload powmod gcd lcm valuation is_prime);
+use Math::AnyNum qw(:overload powmod);
+use ntheory qw(vecprod is_prime gcd valuation);
 
 sub pollard_p1_factor {
     my ($n) = @_;
@@ -22,32 +23,42 @@ sub pollard_p1_factor {
         return ((2) x $v, pollard_p1_factor($n >> $v));
     }
 
-    my ($t, $i, $k) = (2, 1, 1);
+    for (my ($t, $k) = (2, 2) ; ; ++$k) {
 
-    for (;;) {
-
-        my $x = powmod($t, $k, $n);
-        my $g = gcd($x-1, $n);
+        $t = powmod($t, $k, $n);
+        my $g = gcd($t - 1, $n);
 
         if ($g != 1) {
 
             if ($g == $n) {
-                ++$t; next;
+                $t *= $k;
+                next;
             }
 
             return sort { $a <=> $b } (
                 pollard_p1_factor($g),
-                pollard_p1_factor($n/$g)
+                pollard_p1_factor($n / $g)
             );
         }
-
-        $k = lcm($k, ++$i);
     }
 }
 
-say join(', ', pollard_p1_factor(25889*46511));
+say join(', ', pollard_p1_factor(25889 * 46511));
 say join(', ', pollard_p1_factor(419763345));
 say join(', ', pollard_p1_factor(5040));
 say join(', ', pollard_p1_factor(12129569695640600539));
+say join(', ', pollard_p1_factor(2**42 + 1));
 say join(', ', pollard_p1_factor(2**64 + 1));
 say join(', ', pollard_p1_factor(38568900844635025971879799293495379321));
+
+# Run some tests
+foreach my $n (1 .. 10000) {
+    my @factors = pollard_p1_factor($n);
+    if ((grep { is_prime($_) } @factors) != @factors) {
+        die "Composite factor for $n";
+    }
+
+    if (vecprod(@factors) != $n) {
+        die "Incorrect factors for $n";
+    }
+}
