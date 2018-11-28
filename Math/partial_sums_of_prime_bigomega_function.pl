ccb4da90e3315baa064398e5ff3b6638d649f57c
trizen <trizen@protonmail.com>
2018-11-28 06:27:37 +0200

    modified:   Math/partial_sums_of_prime_bigomega_function.pl modified:   Math/partial_sums_of_prime_omega_function.pl

--- Math/partial_sums_of_prime_bigomega_function.pl
+++ Math/partial_sums_of_prime_bigomega_function.pl
@@ -4,45 +4,89 @@
 # Date: 27 November 2018
 # https://github.com/trizen
 
-# A nice algorithm in terms of the prime-counting function for computing partial sums of the bigomega(n) function:
-#   a(n) = Sum_{k=1..n} Ω(k)
+# A nice algorithm in terms of the prime-counting function for computing partial sums of the generalized bigomega(n) function:
+#   B_m(n) = Sum_{k=1..n} Ω_m(k)
 
-# Also equivalent with:
-#   a(n) = bigomega(n!)
+# For `m=0`, we have:
+#   B_0(n) = bigomega(n!)
 
-# See also:
+# OEIS related sequences:
 #   https://oeis.org/A025528
 #   https://oeis.org/A022559
 #   https://oeis.org/A071811
+#   https://oeis.org/A154945  (0.55169329765699918...)
+#   https://oeis.org/A286229  (0.19411816983263379...)
+
+# Example for `B_0(n)`:
+#    B_0(10^1) = 15
+#    B_0(10^2) = 239
+#    B_0(10^3) = 2877
+#    B_0(10^4) = 31985
+#    B_0(10^5) = 343614
+#    B_0(10^6) = 3626619
+#    B_0(10^7) = 37861249
+#    B_0(10^8) = 392351272
+#    B_0(10^9) = 4044220058
+#    B_0(10^10) = 41518796555
+#    B_0(10^11) = 424904645958
+
+# Example for `B_1(n)`:
+#   B_1(10^1) = 30
+#   B_1(10^2) = 2815
+#   B_1(10^3) = 276337
+#   B_1(10^4) = 27591490
+#   B_1(10^5) = 2758525172
+#   B_1(10^6) = 275847515154
+#   B_1(10^7) = 27584671195911
+#   B_1(10^8) = 2758466558498626
+#   B_1(10^9) = 275846649393437566
+#   B_1(10^10) = 27584664891073330599
+#   B_1(10^11) = 2758466488352698209587
+
+# Example for `B_2(n)`:
+#   B_2(10^1) = 82
+#   B_2(10^2) = 66799
+#   B_2(10^3) = 64901405
+#   B_2(10^4) = 64727468210
+#   B_2(10^5) = 64708096890744
+#   B_2(10^6) = 64706281936598588
+#   B_2(10^7) = 64706077322294843451
+#   B_2(10^8) = 64706058761567362618628
+#   B_2(10^9) = 64706056807390376400359474
+#   B_2(10^10) = 64706056632561375736945155965
+#   B_2(10^11) = 64706056612919470606889256184409
+
+# Asymptotic formulas:
+#   B_1(n) ~ 0.55169329765699918... * n*(n+1)/2
+#   B_2(n) ~ 0.19411816983263379... * n*(n-1)*(2*n - 1)/6
+
+# In general, for `m>=1`, we have the following asymptotic formula:
+#   B_m(n) ~ (Sum_{k>=1} primezeta((m+1)*k)) * F_m(n)
+#
+# where F_n(x) is Faulhaber's formula and primezeta(s) is the prime zeta function.
+
+# The prime zeta function is defined as:
+#   primezeta(s) = Sum_{p prime >= 2} 1/p^s
+
+# See also:
+#   https://en.wikipedia.org/wiki/Prime_zeta_function
 #   https://en.wikipedia.org/wiki/Prime_omega_function
 #   https://en.wikipedia.org/wiki/Prime-counting_function
 #   https://trizenx.blogspot.com/2018/11/partial-sums-of-arithmetical-functions.html
-
-# Example:
-#    a(10^1) = 15
-#    a(10^2) = 239
-#    a(10^3) = 2877
-#    a(10^4) = 31985
-#    a(10^5) = 343614
-#    a(10^6) = 3626619
-#    a(10^7) = 37861249
-#    a(10^8) = 392351272
-#    a(10^9) = 4044220058
-#    a(10^10) = 41518796555
-#    a(10^11) = 424904645958
 
 use 5.020;
 use strict;
 use warnings;
 
 use experimental qw(signatures);
-use ntheory qw(vecsum logint sqrtint rootint prime_count forprimes);
+use Math::AnyNum qw(faulhaber_sum);
+use ntheory qw(vecsum logint sqrtint rootint prime_count is_prime_power forprimes);
 
 sub prime_power_count($n) {
     vecsum(map { prime_count(rootint($n, $_)) } 1 .. logint($n, 2));
 }
 
-sub prime_bigomega_partial_sum($n) {
+sub prime_bigomega_partial_sum($n, $m) {
 
     my $s = sqrtint($n);
     my $u = int($n / ($s + 1));
@@ -50,18 +94,51 @@ sub prime_bigomega_partial_sum($n) {
     my $total = 0;
 
     for my $k (1 .. $s) {
-        $total += $k * (prime_power_count(int($n / $k)) - prime_power_count(int($n / ($k + 1))));
+        $total += faulhaber_sum($k, $m) * (prime_power_count(int($n / $k)) - prime_power_count(int($n / ($k + 1))));
     }
 
     forprimes {
         foreach my $k (1 .. logint($u, $_)) {
-            $total += int($n / $_**$k);
+            $total +=faulhaber_sum( int($n / $_**$k), $m);
         }
     } $u;
 
     return $total;
 }
 
-for my $n (1 .. 10) {
-    say "a(10^$n) = ", prime_bigomega_partial_sum(10**$n);
+sub prime_bigomega_partial_sum_test ($n, $m) {      # just for testing
+    my $total = 0;
+
+    foreach my $k (1..$n) {
+        if (is_prime_power($k)) {
+            $total += faulhaber_sum(int($n/$k), $m);
+        }
+    }
+
+    return $total;
 }
+
+for my $m (0 .. 10) {
+
+    my $n = int rand 100000;
+
+    my $t1 = prime_bigomega_partial_sum($n, $m);
+    my $t2 = prime_bigomega_partial_sum_test($n, $m);
+
+    die "error: $t1 != $t2" if ($t1 != $t2);
+
+    say "Sum_{k=1..$n} bigomega_$m(k) = $t1";
+}
+
+__END__
+Sum_{k=1..64129} bigomega_0(k) = 217697
+Sum_{k=1..80658} bigomega_1(k) = 1794616247
+Sum_{k=1..14117} bigomega_2(k) = 182041102184
+Sum_{k=1..42256} bigomega_3(k) = 64820877399946967
+Sum_{k=1..94333} bigomega_4(k) = 54949545016977768030431
+Sum_{k=1..67787} bigomega_5(k) = 280074038628976042168758675
+Sum_{k=1..35346} bigomega_6(k) = 82191526450425222986408201316
+Sum_{k=1..26871} bigomega_7(k) = 138516432841564488200009700415893
+Sum_{k=1..37827} bigomega_8(k) = 35383863032817120893574255077390725080
+Sum_{k=1..75109} bigomega_9(k) = 568264668321999976994584691196910905310669837
+Sum_{k=1..86486} bigomega_10(k) = 90982066598399530764623907560522017063257428908802
--- Math/partial_sums_of_prime_omega_function.pl
+++ Math/partial_sums_of_prime_omega_function.pl
@@ -4,25 +4,68 @@
 # Date: 24 November 2018
 # https://github.com/trizen
 
-# A new algorithm for computing the partial-sums of the prime omega function `ω(k)`, for `1 <= k <= n`:
-#   a(n) = Sum_{k=1..n} ω(k)
+# A new algorithm for computing the partial-sums of the generalized prime omega function `ω_m(k)`, for `1 <= k <= n`:
+#   A_m(n) = Sum_{k=1..n} ω_m(k)
+#
+# where:
+#     ω_m(n) = n^m * Sum_{p|n} 1/p^m
 
 # Based on the formula:
-#   Sum_{k=1..n} ω(k) = Sum_{p prime <= n} floor(n/p)
+#   Sum_{k=1..n} ω_m(k) = Sum_{p prime <= n} F_m(floor(n/p))
+#
+# where F_n(x) is Faulhaber's formula.
 
-# Example:
-#    a(10^1) = 11
-#    a(10^2) = 171
-#    a(10^3) = 2126
-#    a(10^4) = 24300
-#    a(10^5) = 266400
-#    a(10^6) = 2853708
-#    a(10^7) = 30130317
-#    a(10^8) = 315037281
-#    a(10^9) = 3271067968
-#    a(10^10) = 33787242719
-#    a(10^11) = 347589015681
-#    a(10^12) = 3564432632541
+# Example for `m=0`:
+#   A_0(10^1) = 11
+#   A_0(10^2) = 171
+#   A_0(10^3) = 2126
+#   A_0(10^4) = 24300
+#   A_0(10^5) = 266400
+#   A_0(10^6) = 2853708
+#   A_0(10^7) = 30130317
+#   A_0(10^8) = 315037281
+#   A_0(10^9) = 3271067968
+#   A_0(10^10) = 33787242719
+#   A_0(10^11) = 347589015681
+#   A_0(10^12) = 3564432632541
+
+# Example for `m=1`:
+#   A_1(10^1) = 25
+#   A_1(10^2) = 2298
+#   A_1(10^3) = 226342
+#   A_1(10^4) = 22616110
+#   A_1(10^5) = 2261266482
+#   A_1(10^6) = 226124236118
+#   A_1(10^7) = 22612374197143
+#   A_1(10^8) = 2261237139656553
+#   A_1(10^9) = 226123710243814636
+#   A_1(10^10) = 22612371006991736766
+#   A_1(10^11) = 2261237100241987653515
+#   A_1(10^12) = 226123710021083492369813
+
+# Example for `m=2`:
+#   A_2(10^1) = 75
+#   A_2(10^2) = 59962
+#   A_2(10^3) = 58403906
+#   A_2(10^4) = 58270913442
+#   A_2(10^5) = 58255785988898
+#   A_2(10^6) = 58254390385024132
+#   A_2(10^7) = 58254229074894448703
+#   A_2(10^8) = 58254214780225801032503
+#   A_2(10^9) = 58254213248247357411667320
+#   A_2(10^10) = 58254213116747777047390609694
+#   A_2(10^11) = 58254213101385832019517484266265
+#   A_2(10^12) = 58254213099991292350208499967189227
+
+# Asymptotic formulas:
+#   A_1(n) ~ 0.4522474200410654985065... * n*(n+1)/2               (see: https://oeis.org/A085548)
+#   A_2(n) ~ 0.1747626392994435364231... * n*(n-1)*(2*n - 1)/6     (see: https://oeis.org/A085541)
+
+# For `m >= 1`, `A_m(n)` can be described asymptotically in terms of the prime zeta function:
+#   A_m(n) ~ F_m(n) * P(m+1)
+#
+# where P(s) is defined as:
+#   P(s) = Sum_{p prime >= 2} 1/p^s
 
 # See also:
 #   https://oeis.org/A013939
@@ -37,8 +80,9 @@ use warnings;
 
 use experimental qw(signatures);
 use ntheory qw(forprimes prime_count sqrtint);
+use Math::AnyNum qw(faulhaber_sum);
 
-sub prime_omega_partial_sum ($n) {     # O(sqrt(n)) complexity
+sub prime_omega_partial_sum ($n, $m) {     # O(sqrt(n)) complexity
 
     my $total = 0;
 
@@ -46,46 +90,54 @@ sub prime_omega_partial_sum ($n) {     # O(sqrt(n)) complexity
     my $u = int($n / ($s + 1));
 
     for my $k (1 .. $s) {
-        $total += $k * prime_count(int($n/($k+1))+1, int($n/$k));
+        $total += faulhaber_sum($k, $m) * prime_count(int($n/($k+1))+1, int($n/$k));
     }
 
     forprimes {
-        $total += int($n/$_);
+        $total += faulhaber_sum(int($n/$_), $m);
     } $u;
 
     return $total;
 }
 
-sub prime_omega_partial_sum_test ($n) {    # just for testing
+
+sub prime_omega_partial_sum_test ($n, $m) {      # just for testing
     my $total = 0;
 
     forprimes {
-        $total += int($n/$_);
+        $total += faulhaber_sum(int($n/$_), $m);
     } $n;
 
     return $total;
 }
 
-for my $m (1 .. 10) {
-
-    my $n = int rand 100000;
-
-    my $t1 = prime_omega_partial_sum($n);
-    my $t2 = prime_omega_partial_sum_test($n);
-
-    die "error: $t1 != $t2" if ($t1 != $t2);
-
-    say "Sum_{k=1..$n} omega(k) = $t1";
+foreach my $n(1..12) {
+    say "A_2(10^$n) = ", prime_omega_partial_sum(10**$n, 2);
 }
 
 __END__
-Sum_{k=1..62429} omega(k) = 163587
-Sum_{k=1..80890} omega(k) = 213922
-Sum_{k=1..82192} omega(k) = 217486
-Sum_{k=1..97784} omega(k) = 260299
-Sum_{k=1..16940} omega(k) = 42156
-Sum_{k=1..42413} omega(k) = 109555
-Sum_{k=1..18647} omega(k) = 46596
-Sum_{k=1..18716} omega(k) = 46776
-Sum_{k=1..56593} omega(k) = 147768
-Sum_{k=1..65034} omega(k) = 170664
+
+for my $m (0 .. 10) {
+
+    my $n = int rand 100000;
+
+    my $t1 = prime_omega_partial_sum($n, $m);
+    my $t2 = prime_omega_partial_sum_test($n, $m);
+
+    die "error: $t1 != $t2" if ($t1 != $t2);
+
+    say "Sum_{k=1..$n} omega_$m(k) = $t1";
+}
+
+__END__
+Sum_{k=1..93178} omega_0(k) = 247630
+Sum_{k=1..60545} omega_1(k) = 828906439
+Sum_{k=1..61222} omega_2(k) = 13368082621946
+Sum_{k=1..58175} omega_3(k) = 220463446471253532
+Sum_{k=1..26576} omega_4(k) = 94816277435320229002
+Sum_{k=1..17978} omega_5(k) = 96085844643312478233603
+Sum_{k=1..99336} omega_6(k) = 112956550182103434253591001302255
+Sum_{k=1..15217} omega_7(k) = 1459563487599016502195229269710
+Sum_{k=1..62565} omega_8(k) = 3271462737352430519765722633491562894793
+Sum_{k=1..91318} omega_9(k) = 4007044838270388920307792726568428120477189405
+Sum_{k=1..28834} omega_10(k) = 514524955177931497535073881648700561462698676
