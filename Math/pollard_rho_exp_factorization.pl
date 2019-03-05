a4d74cb37c51d083720955e10159e5589f9f549f
trizen <614513+trizen@users.noreply.github.com>
2019-03-05 16:06:49 +0200

    modified:   Math/pollard_rho_exp_factorization.pl -- better polynomial

--- Math/pollard_rho_exp_factorization.pl
+++ Math/pollard_rho_exp_factorization.pl
@@ -3,37 +3,57 @@
 # Pollard's rho integer factorization algorithm.
 
 # This version uses the polynomial:
-#   f(x) = x^(n^2 - 1) + 1
+#   f(x) = x^e + 2*e - 1
+
+# where e = lcm(1..B), for a small bound B.
+
+# See also:
+#   https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm
 
 use 5.020;
 use strict;
 use warnings;
 
-use Math::GMPz;
 use experimental qw(signatures);
 
-sub rho_exp_factor ($n, $max_iter = 20000) {
+use Math::GMPz;
+use Math::Prime::Util::GMP qw(consecutive_integer_lcm logint);
 
-    my $c = 1;
-    my $t = $n * $n - 1;
+sub rho_exp_factor ($n, $max_iter = 5000) {
 
-    my $x = Math::GMPz::Rmpz_init_set_ui($c);
+    my $B = logint($n, 5)**2;
+    my $e = Math::GMPz::Rmpz_init_set_str(consecutive_integer_lcm($B), 10);
+    my $c = 2*$e - 1;
+
+    if (length("$n") <= 12) {
+        $e = Math::GMPz->new(2);
+    }
+
+    my $x = Math::GMPz::Rmpz_init_set_ui(1);
     my $y = Math::GMPz::Rmpz_init();
     my $g = Math::GMPz::Rmpz_init();
 
-    Math::GMPz::Rmpz_powm($y, $x, $t, $n);
-    Math::GMPz::Rmpz_add_ui($y, $y, $c);
+    Math::GMPz::Rmpz_powm($x, $x, $e, $n);
+    Math::GMPz::Rmpz_add($x, $x, $c);
+    Math::GMPz::Rmpz_mod($x, $x, $n);
+
+    Math::GMPz::Rmpz_powm($y, $x, $e, $n);
+    Math::GMPz::Rmpz_add($y, $y, $c);
+    Math::GMPz::Rmpz_mod($y, $y, $n);
 
     for (1 .. $max_iter) {
 
-        Math::GMPz::Rmpz_powm($x, $x, $t, $n);
-        Math::GMPz::Rmpz_add_ui($x, $x, $c);
+        Math::GMPz::Rmpz_powm($x, $x, $e, $n);
+        Math::GMPz::Rmpz_add($x, $x, $c);
+        Math::GMPz::Rmpz_mod($x, $x, $n);
 
-        Math::GMPz::Rmpz_powm($y, $y, $t, $n);
-        Math::GMPz::Rmpz_add_ui($y, $y, $c);
+        Math::GMPz::Rmpz_powm($y, $y, $e, $n);
+        Math::GMPz::Rmpz_add($y, $y, $c);
+        Math::GMPz::Rmpz_mod($y, $y, $n);
 
-        Math::GMPz::Rmpz_powm($y, $y, $t, $n);
-        Math::GMPz::Rmpz_add_ui($y, $y, $c);
+        Math::GMPz::Rmpz_powm($y, $y, $e, $n);
+        Math::GMPz::Rmpz_add($y, $y, $c);
+        Math::GMPz::Rmpz_mod($y, $y, $n);
 
         Math::GMPz::Rmpz_sub($g, $x, $y);
         Math::GMPz::Rmpz_gcd($g, $g, $n);
@@ -48,11 +68,9 @@ sub rho_exp_factor ($n, $max_iter = 20000) {
 }
 
 my @nums = qw(
-  33670570905491953
-  314159265358979323
-  7167393334524676153
-  2559469924891866771047
-  63469917720180180377579
+    314159265358979323 350011490889402191 2954624367769580651
+    7167393334524676153 10033529742475370477 20135752530477192241
+    21316902507352787201 2559469924891866771047 63469917720180180377579
   );
 
 @nums = map { Math::GMPz->new($_) } @nums;
@@ -62,8 +80,12 @@ foreach my $n (@nums) {
 }
 
 __END__
-rho_exp_factor(33670570905491953) = 36169843
-rho_exp_factor(314159265358979323) = 317213509
-rho_exp_factor(7167393334524676153) = 1518057367
+rho_exp_factor(314159265358979323) = 990371647
+rho_exp_factor(350011490889402191) = 692953181
+rho_exp_factor(2954624367769580651) = 490066931
+rho_exp_factor(7167393334524676153) = 4721424559
+rho_exp_factor(10033529742475370477) = 1412164441
+rho_exp_factor(20135752530477192241) = 5907768749
+rho_exp_factor(21316902507352787201) = 3055371353
 rho_exp_factor(2559469924891866771047) = 266349879973
-rho_exp_factor(63469917720180180377579) = 503267186237
+rho_exp_factor(63469917720180180377579) = 126115748167
