6f8893a4ef1dd4aa4f3f30b60ca26fe34ed3c78f
trizen <614513+trizen@users.noreply.github.com>
2019-04-24 19:45:56 +0300

    modified:   Text/markov_chain_text_generator.pl

--- Text/markov_chain_text_generator.pl
+++ Text/markov_chain_text_generator.pl
@@ -2,6 +2,8 @@
 
 # A very simple text generator, using Markov chains.
 
+# This version uses prefixes of variable lengths, between `n_min` and `n_max`.
+
 # See also:
 #   https://en.wikipedia.org/wiki/Markov_chain
 #   https://rosettacode.org/wiki/Markov_chain_text_generator
@@ -12,24 +14,49 @@ use warnings;
 
 use Encode qw(decode_utf8);
 use Text::Unidecode qw(unidecode);
-use List::Util qw(first shuffle);
+use List::Util qw(uniq);
 
-my $n   = 2;
-my $max = 200-$n;
+my $n_min = 2;
+my $n_max = 4;
+my $max   = 200 - $n_max;
 
 sub build_dict {
-    my ($n, @words) = @_;
+    my (@orig_words) = @_;
+
     my %dict;
-    for my $i (0 .. @words - $n - 1) {
-        my @prefix = @words[$i .. $i + $n - 1];
-        push @{$dict{join ' ', @prefix}}, $words[$i + $n];
+
+    foreach my $n ($n_min .. $n_max) {
+
+        my @words = (@orig_words, @orig_words[0 .. $n - 1]);
+
+        for my $i (0 .. $#words - $n) {
+            my @prefix = @words[$i .. $i + $n - 1];
+            push @{$dict{join ' ', @prefix}}, $words[$i + $n];
+        }
     }
+
+    foreach my $key(keys %dict) {
+        $dict{$key} = [uniq(@{$dict{$key}})];
+    }
+
     return %dict;
 }
 
 my $text = do {
-    local $/;
-    <>;
+    if (-t STDIN) {
+        my $content = '';
+        foreach my $file (@ARGV) {
+            open my $fh, '<', $file;
+            local $/;
+            $content .= <$fh>;
+            $content .= "\n";
+        }
+        $content;
+    }
+    else {
+        local $/;
+        <>;
+    }
 };
 
 $text = decode_utf8($text);
@@ -39,24 +66,33 @@ $text = lc($text);
 $text =~ s/[^\w-]+/ /g;
 
 my @words = split ' ', $text;
-push @words, @words[0..$n-1];   # close the loop
 
-my %dict  = build_dict($n, @words);
-my $idx   = int(rand(@words - $n));
-my @rotor = @words[$idx .. $idx + $n - 1];
+my %dict  = build_dict(@words);
+my $idx   = int(rand(@words - $n_max));
+my @rotor = @words[$idx .. $idx + $n_min - 1];
 my @chain = @rotor;
 
 sub pick_next {
     my (@prefix) = @_;
+
     my $key = join(' ', @prefix);
-    shift(@prefix);
     my @arr = @{$dict{$key}};
-    first { exists($dict{join(' ', @prefix, $_)}) } shuffle(@arr);
+
+    $arr[rand @arr];
 }
 
 for (1 .. $max) {
+
     my $new = pick_next(@rotor);
-    shift @rotor;
+    my $idx = int(rand($n_max - $n_min + 1) + $n_min - 1);
+
+    if ($idx > $#rotor) {
+        #shift(@rotor) if rand() < 0.5;
+    }
+    else {
+        @rotor = @rotor[$#rotor - $idx + 1 .. $#rotor];
+    }
+
     push @rotor, $new;
     push @chain, $new;
 }
