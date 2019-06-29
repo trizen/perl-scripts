4b99821b4cd9564fe4540d1996559633d1c25554
trizen <614513+trizen@users.noreply.github.com>
2019-06-29 17:22:21 +0300

    modified:   Math/legendary_question_six.pl

--- Math/legendary_question_six.pl
+++ Math/legendary_question_six.pl
@@ -1,55 +1,68 @@
 #!/usr/bin/perl
 
-# Author: Daniel "Trizen" Șuteu
-# License: GPLv3
-# Date: 18 August 2016
-# Website: https://github.com/trizen
+# Daniel "Trizen" Șuteu
+# Date: 29 June 2019
+# https://github.com/trizen
 
 # The problem:
 #   Let "a" and "b" be positive integers such that a*b + 1 divides a^2 + b^2.
 #   Show that (a^2 + b^2) / (a*b + 1) is the square of an integer.
 
-# See also: https://www.youtube.com/watch?v=Y30VF3cSIYQ
+# This program presents an efficient method for computing non-trivial solutions to the legendary question six.
 
-# This program naively generates solutions for "a" and "b".
+# The Legend of Question Six - Numberphile
+# https://www.youtube.com/watch?v=Y30VF3cSIYQ
 
-use 5.010;
+# The Return of the Legend of Question Six - Numberphile
+# https://www.youtube.com/watch?v=L0Vj_7Y2-xY
+
+# Solutions for (a^2 + b^2) / (1 + ab) = 4, are given by consecutive values of A052530 = { 2, 8, 30, 112, 418, 1560, 5822, ... }.
+
+# Example:
+#   (  2^2 +   8^2) / (    2*8 + 1) = 4
+#   (  8^2 +  30^2) / (   8*30 + 1) = 4
+#   ( 30^2 + 112^2) / ( 30*112 + 1) = 4
+#   (112^2 + 418^2) / (112*418 + 1) = 4
+
+# Similar sequences provide solutions for other values:
+
+# For 3^2: A065100 = { 3, 27, 240, 2133, 18957, 168480, ... }
+# For 4^2: A154021 = { 4, 64, 1020, 16256, 259076, 4128960, ... }
+# For 5^2: A154022 = { 5, 125, 3120, 77875, 1943755, 48516000, ... }
+# For 6^2: A154023 = { 6, 216, 7770, 279504, 10054374, 361677960, ... }
+
+# More generally, let U(n,x) be the Chebyshev polynomials of the second kind, then (a^2 + b^2) / (1 + ab) = m^2, has solutions of the form:
+#
+#   a = m * U(n, m^2 / 2)
+#   b = m * U(n+1, m^2 / 2)
+#
+# for any given positive integer m.
+
+use 5.020;
 use strict;
 use warnings;
 
-use ntheory qw(is_power);
+use experimental qw(signatures);
+use Math::AnyNum qw(:overload chebyshevU);
 
-my $limit = 1e3;
+sub lq6_solutions ($m, $how_many = 5) {
+    map {
+        [$m * chebyshevU($_, $m*$m / 2), $m * chebyshevU($_ + 1, $m*$m / 2)]
+    } 0 .. $how_many-1;
+}
 
-foreach my $x (1 .. $limit) {
-    foreach my $y ($x .. $limit) {
-
-        my $k = ($x**2 + $y**2);
-        my $j = ($x * $y + 1);
-
-        if ($k % $j == 0) {
-            if (is_power($k / $j, 2)) {
-                printf("a = %-10s b = %-10s => %10s / %-10s = %s\n", $x, $y, $k, $j, $k / $j);
-            }
-            else {
-                die "error: found a counter-example...";
-            }
-        }
-    }
+foreach my $k (2 .. 10) {
+    my @S = lq6_solutions($k);
+    say "(a^2 + b^2) / (1 + ab) = $k^2 has solutions: ", join(', ', map { "[@$_]" } @S);
 }
 
 __END__
-a = 1          b = 1          =>          2 / 2          = 1
-a = 2          b = 8          =>         68 / 17         = 4
-a = 3          b = 27         =>        738 / 82         = 9
-a = 4          b = 64         =>       4112 / 257        = 16
-a = 5          b = 125        =>      15650 / 626        = 25
-a = 6          b = 216        =>      46692 / 1297       = 36
-a = 7          b = 343        =>     117698 / 2402       = 49
-a = 8          b = 30         =>        964 / 241        = 4
-a = 8          b = 512        =>     262208 / 4097       = 64
-a = 9          b = 729        =>     531522 / 6562       = 81
-a = 10         b = 1000       =>    1000100 / 10001      = 100
-a = 27         b = 240        =>      58329 / 6481       = 9
-a = 30         b = 112        =>      13444 / 3361       = 4
-a = 112        b = 418        =>     187268 / 46817      = 4
+(a^2 + b^2) / (1 + ab) = 2^2 has solutions: [2 8], [8 30], [30 112], [112 418], [418 1560]
+(a^2 + b^2) / (1 + ab) = 3^2 has solutions: [3 27], [27 240], [240 2133], [2133 18957], [18957 168480]
+(a^2 + b^2) / (1 + ab) = 4^2 has solutions: [4 64], [64 1020], [1020 16256], [16256 259076], [259076 4128960]
+(a^2 + b^2) / (1 + ab) = 5^2 has solutions: [5 125], [125 3120], [3120 77875], [77875 1943755], [1943755 48516000]
+(a^2 + b^2) / (1 + ab) = 6^2 has solutions: [6 216], [216 7770], [7770 279504], [279504 10054374], [10054374 361677960]
+(a^2 + b^2) / (1 + ab) = 7^2 has solutions: [7 343], [343 16800], [16800 822857], [822857 40303193], [40303193 1974033600]
+(a^2 + b^2) / (1 + ab) = 8^2 has solutions: [8 512], [512 32760], [32760 2096128], [2096128 134119432], [134119432 8581547520]
+(a^2 + b^2) / (1 + ab) = 9^2 has solutions: [9 729], [729 59040], [59040 4781511], [4781511 387243351], [387243351 31361929920]
+(a^2 + b^2) / (1 + ab) = 10^2 has solutions: [10 1000], [1000 99990], [99990 9998000], [9998000 999700010], [999700010 99960003000]
