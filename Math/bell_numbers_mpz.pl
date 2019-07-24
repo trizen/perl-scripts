654f4c9c8f060911d08065ad0caf9a97e684e4f2
trizen <trizen@protonmail.com>
2019-07-24 18:13:41 +0300

    modified:   Math/bell_numbers_mpz.pl

--- Math/bell_numbers_mpz.pl
+++ Math/bell_numbers_mpz.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# Fast algorithm for computing the n-th Bell number, using Aitken's array.
+# Fast algorithm for computing the first `n` Bell numbers, using Aitken's array (optimized for space).
 
 # See also:
 #   https://en.wikipedia.org/wiki/Bell_number
@@ -12,30 +12,30 @@ use warnings;
 use Math::GMPz;
 use experimental qw(signatures);
 
-sub bell_number($n) {
+sub bell_numbers($n) {
 
     my @acc;
 
     my $t    = Math::GMPz::Rmpz_init();
-    my $bell = Math::GMPz::Rmpz_init_set_ui(1);
+    my @bell = (Math::GMPz::Rmpz_init_set_ui(1));
 
     foreach my $k (1 .. $n) {
 
-        Math::GMPz::Rmpz_set($t, $bell);
+        Math::GMPz::Rmpz_set($t, $bell[-1]);
 
         foreach my $item (@acc) {
             Math::GMPz::Rmpz_add($t, $t, $item);
             Math::GMPz::Rmpz_set($item, $t);
         }
 
-        unshift @acc, $bell;
-        $bell = Math::GMPz::Rmpz_init_set($acc[-1]);
+        unshift @acc, Math::GMPz::Rmpz_init_set($bell[-1]);
+        push @bell, Math::GMPz::Rmpz_init_set($acc[-1]);
     }
 
-    $bell;
+    @bell;
 }
 
-say join ', ', map { bell_number($_) } 0 .. 15;
+say join ', ', bell_numbers(15);
 
 __END__
 1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147, 115975, 678570, 4213597, 27644437, 190899322, 1382958545
