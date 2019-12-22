86272b4c2055b423dc795287b179d2b71583f250
trizen <trizen@protonmail.com>
2019-12-22 05:49:51 +0200

    new file:   Math/nth_composite.pl

---
+++ Math/nth_composite.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 22 December 2019
+# https://github.com/trizen
+
+# Compute the n-th composite number and the number of composite numbers <= n.
+
+# See also:
+#   https://oeis.org/A002808 -- The composite numbers: numbers n of the form x*y for x > 1 and y > 1.
+#   https://oeis.org/A065857 -- The (10^n)-th composite number.
+
+use 5.020;
+use warnings;
+use ntheory qw(:all);
+use experimental qw(signatures);
+
+sub composite_count($n) {
+    $n - prime_count($n) - 1;
+}
+
+sub nth_composite($n) {
+
+    return undef if ($n <= 0);
+    return 4     if ($n == 1);
+
+    # Lower and upper bounds from A002808 (for n >= 4).
+    my $min = int($n + $n / log($n) + $n / (log($n)**2));
+    my $max = int($n + $n / log($n) + (3 * $n) / (log($n)**2));
+
+    if ($n < 4) {
+        $min = 4;
+        $max = 8;
+    }
+
+    my $k = 0;
+
+    while (1) {
+        $k = ($min + $max) >> 1;
+
+        my $pi  = prime_count($k);
+        my $cmp = ($k <=> ($pi + 1 + $n));
+
+        if ($cmp > 0) {
+            $max = $k - 1;
+        }
+        elsif ($cmp < 0) {
+            $min = $k + 1;
+        }
+        else {
+            last;
+        }
+    }
+
+    --$k if is_prime($k);
+
+    return $k;
+}
+
+say nth_composite(1000000000);      #=> 1053422339
+say composite_count(1053422339);    #=> 1000000000
--- README.md
+++ README.md
@@ -598,6 +598,7 @@ A nice collection of day-to-day Perl scripts.
     * [Next palindrome](./Math/next_palindrome.pl)
     * [Next palindrome in base](./Math/next_palindrome_in_base.pl)
     * [Next power of two](./Math/next_power_of_two.pl)
+    * [Nth composite](./Math/nth_composite.pl)
     * [Nth prime approx](./Math/nth_prime_approx.pl)
     * [Nth root good rational approximations](./Math/nth_root_good_rational_approximations.pl)
     * [Nth root recurrence constant](./Math/nth_root_recurrence_constant.pl)
