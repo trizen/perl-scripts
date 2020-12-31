40fad2186be79fed7fbf82d928ca4f15e729a3e6
trizen <trizen@protonmail.com>
2020-12-31 14:11:46 +0200

    new file:   Math/inverse_of_sigma_function_generalized.pl

---
+++ Math/inverse_of_sigma_function_generalized.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+# Computing the inverse of the sigma_k(n) function, for any k >= 1.
+# Translation of invphi.gp ver. 2.1 by Max Alekseyev.
+
+# See also:
+#   https://home.gwu.edu/~maxal/gpscripts/
+
+use utf8;
+use 5.020;
+use strict;
+use warnings;
+
+use ntheory qw(:all);
+use experimental qw(signatures);
+
+sub dynamicPreimage ($N, $L) {
+
+    my %r = (1 => [1]);
+
+    foreach my $l (@$L) {
+        my %t;
+
+        foreach my $pair (@$l) {
+            my ($x, $y) = @$pair;
+
+            foreach my $d (divisors(divint($N, $x))) {
+                if (exists $r{$d}) {
+                    push @{$t{mulint($x, $d)}}, map { mulint($_, $y) } @{$r{$d}};
+                }
+            }
+        }
+        while (my ($k, $v) = each %t) {
+            push @{$r{$k}}, @$v;
+        }
+    }
+
+    sort { $a <=> $b } @{$r{$N}};
+}
+
+sub cook_sigma ($N, $k) {
+    my %L;
+
+    foreach my $d (divisors($N)) {
+
+        next if ($d == 1);
+
+        foreach my $p (map { $_->[0] } factor_exp(subint($d, 1))) {
+
+            my $q = addint(mulint($d, subint(powint($p, $k), 1)), 1);
+            my $t = valuation($q, $p);
+
+            next if ($t <= $k or ($t % $k) or $q != powint($p, $t));
+
+            push @{$L{$p}}, [$d, powint($p, subint(divint($t, $k), 1))];
+        }
+    }
+
+    [values %L];
+}
+
+sub inverse_sigma ($N, $k = 1) {
+    ($N == 1) ? (1) : dynamicPreimage($N, cook_sigma($N, $k));
+}
+
+say join ', ', inverse_sigma(120);         #=> [54, 56, 87, 95]
+say join ', ', inverse_sigma(22100, 2);    #=> [120, 130, 141]
--- README.md
+++ README.md
@@ -525,6 +525,7 @@ A nice collection of day-to-day Perl scripts.
     * [Inverse of p adic valuation](./Math/inverse_of_p_adic_valuation.pl)
     * [Inverse of sigma function](./Math/inverse_of_sigma_function.pl)
     * [Inverse of sigma function fast](./Math/inverse_of_sigma_function_fast.pl)
+    * [Inverse of sigma function generalized](./Math/inverse_of_sigma_function_generalized.pl)
     * [Inverse of usigma function](./Math/inverse_of_usigma_function.pl)
     * [Invert transform of factorials](./Math/invert_transform_of_factorials.pl)
     * [Is absolute euler pseudoprime](./Math/is_absolute_euler_pseudoprime.pl)
