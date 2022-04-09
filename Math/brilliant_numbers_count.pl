196b0b4b65c7753bfd069c1b9f8a2bfc55b92435
trizen <trizen@protonmail.com>
2022-04-09 13:01:54 +0300

    modified:   Math/brilliant_numbers_count.pl -- even faster

--- Math/brilliant_numbers_count.pl
+++ Math/brilliant_numbers_count.pl
@@ -17,23 +17,51 @@ use warnings;
 use ntheory qw(:all);
 use experimental qw(signatures);
 
-sub brilliant_numbers_count ($n) {
-
-    use integer;
+sub brilliant_numbers_count_fast ($n) {
 
     my $count = 0;
     my $len   = length(sqrtint($n));
 
     foreach my $k (1 .. $len - 1) {
-        my $pi = prime_count(10**($k - 1), 10**$k - 1);
+        my $pi = prime_count(powint(10, $k - 1), powint(10, $k) - 1);
         $count += binomial($pi, 2) + $pi;
     }
 
-    my $min = 10**($len - 1);
-    my $max = 10**$len - 1;
+    my $min = powint(10, $len - 1);
+    my $max = powint(10, $len) - 1;
 
     forprimes {
-        $count += prime_count($_, vecmin($max, $n / $_));
+        $count += prime_count($_, vecmin($max, divint($n, $_)));
+    } $min, $max;
+
+    return $count;
+}
+
+sub brilliant_numbers_count_faster ($n) {
+
+    my $count = 0;
+    my $len   = length(sqrtint($n));
+
+    foreach my $k (1 .. $len - 1) {
+        my $pi = prime_count(powint(10, $k - 1), powint(10, $k) - 1);
+        $count += binomial($pi, 2) + $pi;
+    }
+
+    my $min = powint(10, $len - 1);
+    my $max = powint(10, $len) - 1;
+
+    my $pi_min = prime_count($min);
+    my $pi_max = prime_count($max);
+
+    my $j = -1;
+
+    forprimes {
+        if ($_ * $_ <= $n) {
+            $count += (($n >= $_ * $max) ? $pi_max : prime_count(divint($n, $_))) - $pi_min - ++$j;
+        }
+        else {
+            lastfor;
+        }
     } $min, $max;
 
     return $count;
@@ -60,9 +88,9 @@ sub brilliant_numbers_count_slow ($n) {
     return $count;
 }
 
-foreach my $n (1 .. 11) {
-    my $v = vecprod((10) x $n) - 1;
-    printf("Less than 10^%s, there are %s brilliant numbers\n", $n, brilliant_numbers_count($v));
+foreach my $n (1 .. 12) {
+    my $v = powint(10, $n) - 1;
+    printf("Less than 10^%s, there are %s brilliant numbers\n", $n, brilliant_numbers_count_faster($v));
 }
 
 __END__
@@ -78,3 +106,7 @@ Less than 10^9, there are 7407840 brilliant numbers
 Less than 10^10, there are 35547994 brilliant numbers
 Less than 10^11, there are 491316166 brilliant numbers
 Less than 10^12, there are 2409600865 brilliant numbers
+Less than 10^13, there are 34896253009 brilliant numbers
+Less than 10^14, there are 174155363186 brilliant numbers
+Less than 10^15, there are 2601913448896 brilliant numbers
+Less than 10^16, there are 13163230391312 brilliant numbers
