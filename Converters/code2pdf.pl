aada4d30342672120900c9a6761441c885caafef
trizen <trizen@protonmail.com>
2022-07-30 20:36:50 +0300

    modified:   Converters/code2pdf.pl -- auto-detect the language code from the file extension

--- Converters/code2pdf.pl
+++ Converters/code2pdf.pl
@@ -74,6 +74,165 @@ $tree->eof();
 #my @nodes = $tree->guts();
 my @nodes = $tree->disembowel();
 
+my %language_codes = (
+
+    # Source:
+    #   https://support.codebasehq.com/articles/tips-tricks/syntax-highlighting-in-markdown
+
+    Cucumber     => ['.feature'],
+    abap         => ['.abap'],
+    ada          => ['.adb',      '.ads', '.ada'],
+    ahk          => ['.ahk',      '.ahkl'],
+    apacheconf   => ['.htaccess', 'apache.conf', 'apache2.conf'],
+    applescript  => ['.applescript'],
+    as           => ['.as'],
+    as3          => ['.as'],
+    asy          => ['.asy'],
+    bash         => ['.sh',  '.ksh', '.bash', '.ebuild', '.eclass'],
+    bat          => ['.bat', '.cmd'],
+    befunge      => ['.befunge'],
+    blitzmax     => ['.bmx'],
+    boo          => ['.boo'],
+    brainfuck    => ['.bf',    '.b'],
+    c            => ['.c',     '.h'],
+    cfm          => ['.cfm',   '.cfml', '.cfc'],
+    cheetah      => ['.tmpl',  '.spt'],
+    cl           => ['.cl',    '.lisp', '.el'],
+    clojure      => ['.clj',   '.cljs'],
+    cmake        => ['.cmake', 'CMakeLists.txt'],
+    coffeescript => ['.coffee'],
+    console      => ['.sh-session'],
+    control      => ['control'],
+    cpp          => ['.cpp', '.hpp', '.c++', '.h++', '.cc', '.hh', '.cxx', '.hxx', '.pde'],
+    csharp       => ['.cs'],
+    css          => ['.css'],
+    cython       => ['.pyx', '.pxd', '.pxi'],
+    d            => ['.d',   '.di'],
+    delphi       => ['.pas'],
+    diff         => ['.diff',   '.patch'],
+    dpatch       => ['.dpatch', '.darcspatch'],
+    duel         => ['.duel',   '.jbst'],
+    dylan        => ['.dylan',  '.dyl'],
+    erb          => ['.erb'],
+    erl          => ['.erl-sh'],
+    erlang       => ['.erl', '.hrl'],
+    evoque       => ['.evoque'],
+    factor       => ['.factor'],
+    felix        => ['.flx', '.flxh'],
+    fortran      => ['.f',   '.f90'],
+    gas          => ['.s',   '.S'],
+    genshi       => ['.kid'],
+    glsl         => ['.vert', '.frag', '.geo'],
+    gnuplot      => ['.plot', '.plt'],
+    go           => ['.go'],
+    groff        => ['.1', '.2', '.3', '.4', '.5', '.6', '.7', '.man'],
+    haml         => ['.haml'],
+    haskell      => ['.hs'],
+    html         => ['.html', '.htm', '.xhtml', '.xslt'],
+    hx           => ['.hx'],
+    hybris       => ['.hy',  '.hyb'],
+    ini          => ['.ini', '.cfg'],
+    io           => ['.io'],
+    ioke         => ['.ik'],
+    irc          => ['.weechatlog'],
+    jade         => ['.jade'],
+    java         => ['.java'],
+    js           => ['.js'],
+    jsp          => ['.jsp'],
+    lhs          => ['.lhs'],
+    llvm         => ['.ll'],
+    logtalk      => ['.lgt'],
+    lua          => ['.lua', '.wlua'],
+    make         => ['.mak', 'Makefile', 'makefile', 'Makefile.', 'GNUmakefile'],
+    mako         => ['.mao'],
+    maql         => ['.maql'],
+    mason        => ['.mhtml', '.mc', '.mi', 'autohandler', 'dhandler'],
+    markdown     => ['.md'],
+    modelica     => ['.mo'],
+    modula2      => ['.def', '.mod'],
+    moocode      => ['.moo'],
+    mupad        => ['.mu'],
+    mxml         => ['.mxml'],
+    myghty       => ['.myt', 'autodelegate'],
+    nasm         => ['.asm', '.ASM'],
+    newspeak     => ['.ns2'],
+    objdump      => ['.objdump'],
+    objectivec   => ['.m'],
+    objectivej   => ['.j'],
+    ocaml        => ['.ml', '.mli', '.mll', '.mly'],
+    ooc          => ['.ooc'],
+    perl         => ['.pl',     '.pm',   '.pod'],
+    php          => ['.php',    '.php3', '.php4', '.php5'],
+    postscript   => ['.ps',     '.eps'],
+    pot          => ['.pot',    '.po'],
+    pov          => ['.pov',    '.inc'],
+    prolog       => ['.prolog', '.pro'],
+    properties   => ['.properties'],
+    protobuf     => ['.proto'],
+    py3tb        => ['.py3tb'],
+    pytb         => ['.pytb'],
+    python       => ['.py', '.pyw', '.sc', 'SConstruct', 'SConscript', '.tac'],
+    ruby         => ['.rb', '.rbw', 'Rakefile', '.rake', '.gemspec', '.rbx', '.duby', '.sf', '.sm'],
+    rconsole     => ['.Rout'],
+    rebol        => ['.r', '.r3'],
+    redcode      => ['.cw'],
+    rhtml        => ['.rhtml'],
+    rst          => ['.rst', '.rest'],
+    sass         => ['.sass'],
+    scala        => ['.scala'],
+    scaml        => ['.scaml'],
+    scheme       => ['.scm'],
+    scss         => ['.scss'],
+    smalltalk    => ['.st'],
+    smarty       => ['.tpl'],
+    sourceslist  => ['sources.list'],
+    splus        => ['.S', '.R'],
+    sql          => ['.sql'],
+    sqlite3      => ['.sqlite3-console'],
+    squidconf    => ['squid.conf'],
+    ssp          => ['.ssp'],
+    tcl          => ['.tcl'],
+    tcsh         => ['.tcsh', '.csh'],
+    tex          => ['.tex',  '.aux', '.toc'],
+    text         => ['.txt'],
+    v            => ['.v',    '.sv'],
+    vala         => ['.vala', '.vapi'],
+    vbnet        => ['.vb',   '.bas'],
+    velocity     => ['.vm',   '.fhtml'],
+    vim          => ['.vim',  '.vimrc'],
+    xml          => ['.xml',  '.xsl', '.rss', '.xslt', '.xsd', '.wsdl'],
+    xquery       => ['.xqy',  '.xquery'],
+    xslt         => ['.xsl',  '.xslt'],
+    yaml         => ['.yaml', '.yml'],
+    julia        => ['.jl'],
+                     );
+
+sub determine_language_code {
+    my ($file) = @_;
+
+    my @found_codes;
+
+    foreach my $lang_code (keys %language_codes) {
+        foreach my $ext (@{$language_codes{$lang_code}}) {
+            if (substr($file, -length($ext)) eq $ext) {
+                push @found_codes, $lang_code;
+            }
+        }
+    }
+
+    if (scalar(@found_codes) == 1) {
+        return $found_codes[0];
+    }
+
+    if (scalar(@found_codes) > 1) {
+        warn ":: Ambiguous file extension for <<$file>>: it could be (@found_codes)\n";
+        @found_codes = sort(@found_codes);    # be deterministic
+        return $found_codes[0];
+    }
+
+    return $lang;
+}
+
 say ":: Reading Markdown files...";
 my $markdown_content = '';
 
@@ -105,8 +264,9 @@ sub expand_ul {
 
                         if (open my $fh, '<:utf8', $file) {
                             local $/;
+                            my $lang_code = determine_language_code($file);
                             $markdown_content .= ("#" x $depth) . ' ' . $x->content->[0] . "\n\n";
-                            $markdown_content .= "```$lang\n";
+                            $markdown_content .= "```$lang_code\n";
                             $markdown_content .= <$fh>;
                             if (substr($markdown_content, -1) ne "\n") {
                                 $markdown_content .= "\n";
