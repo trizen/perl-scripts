8dba0184ed5325fd63dfa9c9f74604cc229413bb
trizen <trizen@protonmail.com>
2022-08-01 20:14:01 +0300

    modified:   Image/collage.pl -- added support for rectangular tiles

--- Image/collage.pl
+++ Image/collage.pl
@@ -21,6 +21,8 @@ use Getopt::Long qw(GetOptions);
 GD::Image->trueColor(1);
 
 my $size        = 350;
+my $wsize       = undef;
+my $hsize       = undef;
 my $wcrop       = 1 / 2;           # width crop ratio
 my $hcrop       = 1 / 5;           # height crop ratio
 my $output_file = 'collage.png';
@@ -34,21 +36,25 @@ sub usage {
 usage: $0 [options] [files / directories]
 
 options:
-    --size=i   : the size of a mosaic square (default: $size)
+    --size=i   : the length of a square tile (default: $size)
+    --wsize=i  : the width of a tile (default: $size)
+    --hsize=i  : the height of a tile (default: $size)
     --wcrop=f  : width cropping ratio (default: $wcrop)
     --hcrop=f  : height cropping ratio (default: $hcrop)
-    --width=i  : width of the collage (default: auto)
-    --height=i : height of the collage (default: auto)
+    --width=i  : minimum width of the collage (default: auto)
+    --height=i : minimum height of the collage (default: auto)
     --output=s : output filename (default: $output_file)
 
 example:
-    $0 --size=20 ~/Pictures
+    $0 --size=100 ~/Pictures
 EOT
     exit($code);
 }
 
 GetOptions(
            'size=i'   => \$size,
+           'wsize=i'  => \$wsize,
+           'hsize=i'  => \$hsize,
            'wcrop=f'  => \$wcrop,
            'hcrop=f'  => \$hcrop,
            'width=i'  => \$width,
@@ -77,12 +83,12 @@ sub resize_image {
     my ($width, $height) = $image->getBounds();
 
     # File is already at the wanted resolution
-    if ($width == $size and $height == $size) {
+    if ($width == $wsize and $height == $hsize) {
         return $image;
     }
 
     # Get the minimum ratio
-    my $min_r = min($width / $size, $height / $size);
+    my $min_r = min($width / $wsize, $height / $hsize);
 
     my $n_width  = sprintf('%.0f', $width / $min_r);
     my $n_height = sprintf('%.0f', $height / $min_r);
@@ -92,20 +98,20 @@ sub resize_image {
     $gd->copyResampled($image, 0, 0, 0, 0, $n_width, $n_height, $width, $height);
 
     # Create a new GD image with the wanted dimensions
-    my $cropped = GD::Image->new($size, $size);
+    my $cropped = GD::Image->new($wsize, $hsize);
 
     # Crop from left and right
-    if ($n_width > $size) {
-        my $diff = $n_width - $size;
+    if ($n_width > $wsize) {
+        my $diff = $n_width - $wsize;
         my $left = ceil($diff * $wcrop);
-        $cropped->copy($gd, 0, 0, $left, 0, $size, $size);
+        $cropped->copy($gd, 0, 0, $left, 0, $wsize, $hsize);
     }
 
     # Crop from top and bottom
-    elsif ($n_height > $size) {
-        my $diff = $n_height - $size;
+    elsif ($n_height > $hsize) {
+        my $diff = $n_height - $hsize;
         my $top  = int($diff * $hcrop);
-        $cropped->copy($gd, 0, 0, 0, $top, $size, $size);
+        $cropped->copy($gd, 0, 0, 0, $top, $wsize, $hsize);
     }
 
     # No crop needed
@@ -118,8 +124,11 @@ sub resize_image {
 
 my @photo_dirs = (@ARGV ? @ARGV : usage(2));
 
-if ($size <= 0) {
-    die "$0: size must be greater than zero (got: $size)\n";
+$wsize //= $size;
+$hsize //= $size;
+
+if ($wsize <= 0 or $hsize <= 0) {
+    die "$0: size must be greater than zero (got: [$size, $wsize, $hsize])\n";
 }
 
 my @images;    # stores all the image objects
@@ -135,31 +144,31 @@ find {
 
 my $images_len = scalar(@images);
 
-$width  //= int(sqrt($images_len)) * $size;
+$width  //= int(sqrt($images_len)) * $wsize;
 $height //= $width;
 
-if ($width % $size != 0) {
-    $width += ($size - ($width % $size));
+if ($width % $wsize != 0) {
+    $width += ($wsize - ($width % $wsize));
 }
 
-if ($height % $size != 0) {
-    $height += ($size - ($height % $size));
+if ($height % $hsize != 0) {
+    $height += ($hsize - ($height % $hsize));
 }
 
-while (($width / $size) * ($height / $size) > $images_len) {
-    $height -= $size;
+while (($width / $wsize) * ($height / $hsize) > $images_len) {
+    $height -= $hsize;
 }
 
-while (($width / $size) * ($height / $size) < $images_len) {
-    $height += $size;
+while (($width / $wsize) * ($height / $hsize) < $images_len) {
+    $height += $hsize;
 }
 
 my $collage = GD::Image->new($width, $height);
 
-foreach my $y (0 .. $height / $size - 1) {
-    foreach my $x (0 .. $width / $size - 1) {
+foreach my $y (0 .. $height / $hsize - 1) {
+    foreach my $x (0 .. $width / $wsize - 1) {
         my $source = shift(@images) // last;
-        $collage->copy($source, $x * $size, $y * $size, 0, 0, $size, $size);
+        $collage->copy($source, $x * $wsize, $y * $hsize, 0, 0, $wsize, $hsize);
     }
 }
 
