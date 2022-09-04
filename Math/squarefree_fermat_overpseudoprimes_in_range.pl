969f06cfdf04e9bbb4e9f25625f4e39446c80c04
trizen <trizen@protonmail.com>
2022-09-04 20:31:36 +0300

    modified:   Math/squarefree_fermat_overpseudoprimes_in_range.pl -- simplifications

--- Math/squarefree_fermat_overpseudoprimes_in_range.pl
+++ Math/squarefree_fermat_overpseudoprimes_in_range.pl
@@ -2,7 +2,7 @@
 
 # Daniel "Trizen" Șuteu
 # Date: 28 August 2022
-# Edit: 03 September 2022
+# Edit: 04 September 2022
 # https://github.com/trizen
 
 # Generate all the squarefree Fermat overpseudoprimes to given a base with n prime factors in a given range [a,b]. (not in sorted order)
@@ -14,123 +14,96 @@
 use 5.020;
 use warnings;
 
-use ntheory qw(:all);
+use ntheory      qw(:all);
 use experimental qw(signatures);
-use Memoize qw(memoize);
+use Memoize      qw(memoize);
 
 memoize('inverse_znorder_primes');
 
-sub divceil ($x,$y) {   # ceil(x/y)
+sub divceil ($x, $y) {    # ceil(x/y)
     my $q = divint($x, $y);
-    ($q*$y == $x) ? $q : ($q+1);
+    ($q * $y == $x) ? $q : ($q + 1);
 }
 
-sub inverse_znorder_primes($base, $lambda) {
+sub inverse_znorder_primes ($base, $lambda) {
     my %seen;
-    grep { znorder($base, $_) == $lambda } grep { !$seen{$_}++ } factor(subint(powint($base, $lambda), 1));
+    grep { !$seen{$_}++ } factor(subint(powint($base, $lambda), 1));
+}
+
+sub iterate_over_primes ($x, $y, $base, $lambda, $callback) {
+
+    if ($lambda > 1 and $lambda <= 135) {
+        foreach my $p (inverse_znorder_primes($base, $lambda)) {
+
+            next if $p < $x;
+            next if $p > $y;
+
+            #znorder($base, $p) == $lambda or next;
+
+            $callback->($p);
+        }
+        return;
+    }
+
+    if ($lambda > 1) {
+        for (my $w = $lambda * divceil($x - 1, $lambda) ; $w <= $y ; $w += $lambda) {
+            if (is_prime($w + 1) and powmod($base, $lambda, $w + 1) == 1) {
+                $callback->($w + 1);
+            }
+        }
+        return;
+    }
+
+    for (my $p = next_prime($x - 1) ; $p <= $y ; $p = next_prime($p)) {
+        $callback->($p);
+    }
 }
 
 sub squarefree_fermat_overpseudoprimes_in_range ($A, $B, $k, $base, $callback) {
 
     $A = vecmax($A, pn_primorial($k));
 
-    sub ($m, $lambda, $p, $k, $u = undef, $v = undef) {
+    my $F;
+    $F = sub ($m, $lambda, $x, $k, $u = undef, $v = undef) {
 
         if ($k == 1) {
 
-            if ($lambda <= 135) {
-                foreach my $p (inverse_znorder_primes($base, $lambda)) {
-                    next if $p < $u;
-                    next if $p > $v;
-                    if (($m*$p - 1)%$lambda == 0) {
-                        $callback->($m*$p);
+            iterate_over_primes($u, $v, $base, $lambda, sub ($p) {
+                if (powmod($base, $lambda, $p) == 1) {
+                    if (($m * $p - 1) % $lambda == 0 and znorder($base, $p) == $lambda) {
+                        $callback->($m * $p);
                     }
                 }
-                return;
-            }
-
-            if (prime_count_lower($v)-prime_count_lower($u) < divint($v-$u, $lambda)) {
-                forprimes {
-                    if (($m*$_ - 1)%$lambda == 0 and powmod($base, $lambda, $_) == 1 and znorder($base, $_) == $lambda) {
-                        $callback->($m*$_);
-                    }
-                } $u, $v;
-                return;
-            }
-
-            for(my $w = $lambda * divceil($u-1, $lambda); $w <= $v; $w += $lambda) {
-                if (is_prime($w+1) and powmod($base, $lambda, $w+1) == 1) {
-                    my $p = $w+1;
-                    if (($m*$p - 1)%$lambda == 0 and znorder($base, $p) == $lambda) {
-                        $callback->($m*$p);
-                    }
-                }
-            }
+            });
 
             return;
         }
 
-        my $s = rootint(divint($B, $m), $k);
+        my $y = rootint(divint($B, $m), $k);
 
-        if ($lambda > 1 and $lambda <= 135) {
-            for my $q (inverse_znorder_primes($base, $lambda)) {
+        $x > $y and return;
 
-                next if ($q < $p);
-                next if ($q > $s);
+        iterate_over_primes($x, $y, $base, $lambda, sub ($p) {
+            if ($base % $p != 0) {
 
-                my $t = $m*$q;
-                my $u = divceil($A, $t);
-                my $v = divint($B, $t);
+                my $L = znorder($base, $p);
+                if (($L == $lambda or $lambda == 1) and gcd($L, $m) == 1) {
 
-                if ($u <= $v) {
-                    my $r = next_prime($q);
-                    __SUB__->($t, $lambda, $r, $k-1, (($k==2 && $r>$u) ? $r : $u), $v);
-                }
-            }
-            return;
-        }
-
-        if ($lambda > 1) {
-            for(my $w = $lambda * divceil($p-1, $lambda); $w <= $s; $w += $lambda) {
-                if (is_prime($w+1) and powmod($base, $lambda, $w+1) == 1) {
-                    my $p = $w+1;
-
-                    $lambda == znorder($base, $p) or next;
-                    $base % $p == 0 and next;
-
-                    my $t = $m*$p;
+                    my $t = $m * $p;
                     my $u = divceil($A, $t);
                     my $v = divint($B, $t);
 
                     if ($u <= $v) {
                         my $r = next_prime($p);
-                        __SUB__->($t, $lambda, $r, $k-1, (($k==2 && $r>$u) ? $r : $u), $v);
+                        $F->($t, $L, $r, $k - 1, (($k == 2 && $r > $u) ? $r : $u), $v);
                     }
                 }
             }
+        });
+    };
 
-            return;
-        }
-
-        for (my $r; $p <= $s; $p = $r) {
-
-            $r = next_prime($p);
-            $base % $p == 0 and next;
-
-            my $L = znorder($base, $p);
-            $L == $lambda or $lambda == 1 or next;
-
-            gcd($L, $m) == 1 or next;
-
-            my $t = $m*$p;
-            my $u = divceil($A, $t);
-            my $v = divint($B, $t);
-
-            if ($u <= $v) {
-                __SUB__->($t, $L, $r, $k - 1, (($k==2 && $r>$u) ? $r : $u), $v);
-            }
-        }
-    }->(1, 1, 2, $k);
+    $F->(1, 1, 2, $k);
+    undef $F;
 }
 
 # Generate all the squarefree Fermat overpseudoprimes to base 2 with 3 prime factors in the range [13421773, 412346200100]
