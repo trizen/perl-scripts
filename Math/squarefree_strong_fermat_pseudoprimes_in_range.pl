5663b5db4004ea20714865e1b82667337009cdbf
trizen <trizen@protonmail.com>
2022-09-24 19:37:18 +0300

    new file:   Math/lucas-carmichael_numbers_in_range_from_prime_factors.pl new file:   Math/squarefree_strong_fermat_pseudoprimes_in_range.pl new file:   Math/strong_fermat_pseudoprimes_in_range.pl

---
+++ Math/squarefree_strong_fermat_pseudoprimes_in_range.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 24 September 2022
+# https://github.com/trizen
+
+# Generate all the squarefree strong Fermat pseudoprimes to given a base with n prime factors in a given range [A,B]. (not in sorted order)
+
+# See also:
+#   https://en.wikipedia.org/wiki/Almost_prime
+#   https://trizenx.blogspot.com/2020/08/pseudoprimes-construction-methods-and.html
+
+use 5.020;
+use warnings;
+
+use ntheory      qw(:all);
+use experimental qw(signatures);
+
+sub divceil ($x, $y) {    # ceil(x/y)
+    my $q = divint($x, $y);
+    ($q * $y == $x) ? $q : ($q + 1);
+}
+
+sub squarefree_strong_fermat_pseudoprimes_in_range ($A, $B, $k, $base, $callback) {
+
+    $A = vecmax($A, pn_primorial($k));
+
+    my $generator = sub ($m, $lambda, $p, $k, $k_exp, $congr, $u = undef, $v = undef) {
+
+        if ($k == 1) {
+
+            forprimes {
+                my $valuation = valuation($_ - 1, 2);
+                if ($valuation > $k_exp
+                    and powmod($base, (($_ - 1) >> $valuation) << $k_exp, $_) == ($congr % $_)) {
+                    my $t = $m * $_;
+                    if (($t - 1) % $lambda == 0 and ($t - 1) % znorder($base, $_) == 0) {
+                        $callback->($t);
+                    }
+                }
+            } $u, $v;
+
+            return;
+        }
+
+        my $s = rootint(divint($B, $m), $k);
+
+        for (my $r ; $p <= $s ; $p = $r) {
+
+            $r = next_prime($p);
+            $base % $p == 0 and next;
+
+            my $valuation = valuation($p - 1, 2);
+            $valuation > $k_exp                                                    or next;
+            powmod($base, (($p - 1) >> $valuation) << $k_exp, $p) == ($congr % $p) or next;
+
+            my $z = znorder($base, $p);
+            my $L = lcm($lambda, $z);
+
+            gcd($L, $m) == 1 or next;
+
+            my $t = $m * $p;
+            my $u = divceil($A, $t);
+            my $v = divint($B, $t);
+
+            if ($u <= $v) {
+                __SUB__->($t, $L, $r, $k - 1, $k_exp, $congr, (($k == 2 && $r > $u) ? $r : $u), $v);
+            }
+        }
+    };
+
+    # Case where 2^d == 1 (mod p), where d is the odd part of p-1.
+    $generator->(1, 1, 2, $k, 0, 1);
+
+    # Cases where 2^(d * 2^v) == -1 (mod p), for some v >= 0.
+    foreach my $v (0 .. logint($B, 2)) {
+        $generator->(1, 1, 2, $k, $v, -1);
+    }
+}
+
+# Generate all the squarefree strong Fermat pseudoprimes to base 2 with 3 prime factors in the range [1, 10^7]
+
+my $k    = 3;
+my $base = 2;
+my $from = 1;
+my $upto = 1e7;
+
+my @arr;
+squarefree_strong_fermat_pseudoprimes_in_range($from, $upto, $k, $base, sub ($n) { push @arr, $n });
+
+say join(', ', sort { $a <=> $b } @arr);
+
+__END__
+15841, 29341, 52633, 74665, 252601, 314821, 476971, 635401, 1004653, 1023121, 1907851, 1909001, 2419385, 2953711, 3581761, 4335241, 4682833, 5049001, 5444489, 5599765, 5681809, 9069229
---
+++ Math/strong_fermat_pseudoprimes_in_range.pl
@@ -0,0 +1,98 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 24 September 2022
+# https://github.com/trizen
+
+# Generate all the k-omega strong Fermat pseudoprimes in range [A,B]. (not in sorted order)
+
+# Definition:
+#   k-omega primes are numbers n such that omega(n) = k.
+
+# See also:
+#   https://en.wikipedia.org/wiki/Almost_prime
+#   https://en.wikipedia.org/wiki/Prime_omega_function
+#   https://trizenx.blogspot.com/2020/08/pseudoprimes-construction-methods-and.html
+
+# PARI/GP program (version 1):
+#   strong_fermat_psp(A, B, k, base) = A=max(A, vecprod(primes(k))); (f(m, l, p, j, k_exp, congr) = my(list=List()); forprime(q=p, sqrtnint(B\m, j), if(base%q != 0, my(tv=valuation(q-1, 2)); if(tv > k_exp && Mod(base, q)^(((q-1)>>tv)<<k_exp) == congr, my(v=m*q, t=q, r=nextprime(q+1)); while(v <= B, my(L=lcm(l, znorder(Mod(base, t)))); if(gcd(L, v) == 1, if(j==1, if(v>=A && if(k==1, !isprime(v), 1) && (v-1)%L == 0, listput(list, v)), if(v*r <= B, list=concat(list, f(v, L, r, j-1, k_exp, congr)))), break); v *= q; t *= q)))); list); my(r=f(1, 1, 2, k, 0, 1)); for(v=0, logint(B, 2), r=concat(r, f(1, 1, 2, k, v, -1))); vecsort(Vec(r));
+
+# PARI/GP program (version 2):
+#   strong_fermat_psp(A, B, k, base) = A=max(A, vecprod(primes(k))); (f(m, l, p, j, k_exp, congr) = my(list=List()); forprime(q=p, sqrtnint(B\m, j), if(base%q != 0, my(tv=valuation(q-1, 2)); if(tv > k_exp && Mod(base, q)^(((q-1)>>tv)<<k_exp) == congr, my(v=m*q, t=q, r=nextprime(q+1)); while(v <= B, my(L=lcm(l, znorder(Mod(base, t)))); if(gcd(L, v) == 1, my(tv=valuation(t-1, 2)); if(tv > k_exp && Mod(base, t)^(((t-1)>>tv)<<k_exp) == congr, if(j==1, if(v>=A && if(k==1, !isprime(v), 1) && (v-1)%L == 0, listput(list, v)), if(v*r <= B, list=concat(list, f(v, L, r, j-1, k_exp, congr))))), break); v *= q; t *= q)))); list); my(r=f(1, 1, 2, k, 0, 1)); for(v=0, logint(B, 2), r=concat(r, f(1, 1, 2, k, v, -1))); vecsort(Vec(r));
+
+use 5.020;
+use warnings;
+
+use ntheory      qw(:all);
+use experimental qw(signatures);
+
+sub strong_fermat_pseudoprimes_in_range ($A, $B, $k, $base, $callback) {
+
+    $A = vecmax($A, pn_primorial($k));
+    $A > $B and return;
+
+    my $generator = sub ($m, $lambda, $p, $j, $k_exp, $congr) {
+
+        my $s = rootint(divint($B, $m), $j);
+
+        for (my $r ; $p <= $s ; $p = $r) {
+
+            $r = next_prime($p);
+
+            if ($base % $p == 0) {
+                next;
+            }
+
+            my $valuation = valuation($p - 1, 2);
+            $valuation > $k_exp                                                          or next;
+            powmod($base, (($p - 1) >> $valuation) << $k_exp, $p) == ($congr % $p) or next;
+
+            for (my ($q, $v) = ($p + 0, $m * $p) ; $v <= $B ; ($q, $v) = ($q * $p, $v * $p)) {
+
+                my $L = lcm($lambda, znorder($base, $q));
+
+                gcd($L, $v) == 1 or last;
+
+                my $valuation = valuation($q - 1, 2);
+                $valuation > $k_exp                                                          or next;
+                powmod($base, (($q - 1) >> $valuation) << $k_exp, $q) == ($congr % $q) or next;
+
+                if ($j == 1) {
+                    $v >= $A or next;
+                    $k == 1 and is_prime($v) and next;
+                    ($v - 1) % $L == 0 or next;
+                    $callback->($v);
+                    next;
+                }
+
+                $v * $r <= $B or next;
+                __SUB__->($v, $L, $r, $j - 1, $k_exp, $congr);
+            }
+        }
+    };
+
+    # Case where 2^d == 1 (mod p), where d is the odd part of p-1.
+    $generator->(1, 1, 2, $k, 0, 1);
+
+    # Cases where 2^(d * 2^v) == -1 (mod p), for some v >= 0.
+    foreach my $v (0 .. logint($B, 2)) {
+        $generator->(1, 1, 2, $k, $v, -1);
+    }
+}
+
+# Generate all the Fermat pseudoprimes to base 3 in range [1, 10^5]
+
+my $from = 1;
+my $upto = 1e5;
+my $base = 3;
+
+my @arr;
+foreach my $k (1 .. 100) {
+    last if pn_primorial($k) > $upto;
+    strong_fermat_pseudoprimes_in_range($from, $upto, $k, $base, sub ($n) { push @arr, $n });
+}
+
+say join(', ', sort { $a <=> $b } @arr);
+
+__END__
+121, 703, 1891, 3281, 8401, 8911, 10585, 12403, 16531, 18721, 19345, 23521, 31621, 44287, 47197, 55969, 63139, 74593, 79003, 82513, 87913, 88573, 97567
