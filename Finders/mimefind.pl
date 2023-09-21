2132ea1240250b675d048092f4d1b6c2dd25fc62
trizen <trizenx@tutanota.com>
2023-09-21 18:30:07 +0300

    modified:   Finders/mimefind.pl -- added support for `-B` (binary files only)` and `-T` (text files only).

--- Finders/mimefind.pl
+++ Finders/mimefind.pl
@@ -2,6 +2,7 @@
 
 # Author: Trizen
 # Date: 17 April 2023
+# Edit: 21 September 2023
 # https://github.com/trizen
 
 # Find files from a given directory (and its subdirectories) that have a specific mimetype.
@@ -16,6 +17,8 @@ usage: $0 [options] [files | dirs]
 
 options:
 
+    -T       : display only text files
+    -B       : display only binary files
     -t TYPE  : display files with this mimetype (regex)
     -n       : display non-matching files
     -f       : display only files
@@ -25,6 +28,7 @@ options:
 examples:
 
     perl $0 -t video ~/Music              # find video files
+    perl $0 -Bft . ~/Documents            # find binary files
     perl $0 -fn -t audio ~/Music          # find non-audio files
     perl $0 -fn -t 'audio|video' ~/Music  # find non audio/video files
 
@@ -32,11 +36,11 @@ EOT
     exit($exit_code);
 }
 
-getopts('efhnt:', \my %opts);
+getopts('TBefhnt:', \my %opts);
 $opts{t} || usage(1);
 $opts{h} && usage(0);
 
-my $type_re = qr/$opts{t}/;
+my $type_re = qr/$opts{t}/i;
 
 sub determine_mime_type ($file) {
 
@@ -62,11 +66,22 @@ find(
     {
      wanted => sub {
 
+         if ($opts{f}) {
+             (-f $_) or return;
+         }
+
+         if ($opts{B}) {
+             (-B $_) or return;
+         }
+
+         if ($opts{T}) {
+             (-T $_) or return;
+         }
+
          my $mimetype = determine_mime_type($_) // return;
          my $ok       = ($mimetype =~ $type_re);
 
          $ok = !$ok if $opts{n};
-         $ok = 0    if ($opts{f} and not -f $_);
 
          if ($ok) {
              say $File::Find::name;
