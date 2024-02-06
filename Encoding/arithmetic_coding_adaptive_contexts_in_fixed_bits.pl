bf6d6f527c651cb6a478428abfbaf0eacc5c85bd
trizen <trizenx@tutanota.com>
2024-02-06 16:32:12 +0200

    modified:   Encoding/arithmetic_coding_adaptive_contexts_in_fixed_bits.pl -- better implementation of the Arithmetic Coding in fixed bits

--- Encoding/arithmetic_coding_adaptive_contexts_in_fixed_bits.pl
+++ Encoding/arithmetic_coding_adaptive_contexts_in_fixed_bits.pl
@@ -2,6 +2,7 @@
 
 # Author: Trizen
 # Date: 12 July 2023
+# Edit: 06 February 2024
 # https://github.com/trizen
 
 # The Arithmetic Coding algorithm (adaptive version), implemented using native integers.
@@ -15,15 +16,13 @@
 
 use 5.036;
 
-use constant {BITS => 31};
-
 use constant {
-              MAX    => (1 << BITS) - 1,
-              MSB    => (1 << (BITS - 1)),
-              SMSB   => (1 << (BITS - 2)),
               ESCAPE => 256,
               EOF    => 257,
              };
+
+use constant BITS => 32;
+use constant MAX  => oct('0b' . ('1' x BITS));
 
 sub create_cfreq ($table) {
 
@@ -56,7 +55,7 @@ sub create_contexts {
                        [(map { [$_, 1 - $i] } 0 .. 255),
                         (
                            ($i == 0)
-                         ? ([ESCAPE, 1], [EOF, 1])
+                         ? ([ESCAPE, 0], [EOF, 1])
                          : ([ESCAPE, 1], [EOF, 1])
                         ),
                        ]
@@ -108,8 +107,8 @@ sub encode ($string) {
     my sub encode_symbol ($c, $context) {
 
         my $w = $C[$context]{high} - $C[$context]{low} + 1;
-        $C[$context]{high} = $C[$context]{low} + int(($w * $C[$context]{cf_high}->{$c}) / $C[$context]{T});
-        $C[$context]{low}  = $C[$context]{low} + int(($w * $C[$context]{cf_low}->{$c}) / $C[$context]{T});
+        $C[$context]{high} = ($C[$context]{low} + int(($w * $C[$context]{cf_high}->{$c}) / $C[$context]{T}) - 1) & MAX;
+        $C[$context]{low}  = ($C[$context]{low} + int(($w * $C[$context]{cf_low}->{$c}) / $C[$context]{T})) & MAX;
 
         foreach my $context (1) {
             $C[$context]{T} = increment_freq($c, $C[$context]{freq}, $C[$context]{cf_low}, $C[$context]{cf_high});
@@ -125,47 +124,35 @@ sub encode ($string) {
 
         while (1) {
 
-            if (($C[$context]{low} & MSB) == ($C[$context]{high} & MSB)) {
+            if (($C[$context]{high} >> (BITS - 1)) == ($C[$context]{low} >> (BITS - 1))) {
 
-                my $bit = ($C[$context]{low} & MSB) >> (BITS - 1);
-
+                my $bit = $C[$context]{high} >> (BITS - 1);
                 $enc .= $bit;
 
                 if ($C[$context]{uf_count} > 0) {
-                    $enc .= join('', (1 - $bit) x $C[$context]{uf_count});
+                    $enc .= join('', 1 - $bit) x $C[$context]{uf_count};
                     $C[$context]{uf_count} = 0;
                 }
 
-                if ($bit == 1) {
-                    $C[$context]{low}  ^= MSB;
-                    $C[$context]{high} ^= MSB;
-                }
-
-                $C[$context]{low}  <<= 1;
-                $C[$context]{high} <<= 1;
-                $C[$context]{high} |= 1;
+                $C[$context]{low} <<= 1;
+                ($C[$context]{high} <<= 1) |= 1;
             }
-            elsif ((($C[$context]{low} & SMSB) == SMSB) and (($C[$context]{high} & SMSB) == 0)) {
-
-                $C[$context]{low} ^= SMSB;
-                $C[$context]{high} -= SMSB if ($C[$context]{high} >= SMSB);
-
-                $C[$context]{low}  <<= 1;
-                $C[$context]{high} <<= 1;
+            elsif (((($C[$context]{low} >> (BITS - 2)) & 0x1) == 1) && ((($C[$context]{high} >> (BITS - 2)) & 0x1) == 0)) {
+                ($C[$context]{high} <<= 1) |= (1 << (BITS - 1));
                 $C[$context]{high} |= 1;
-
-                $C[$context]{uf_count} += 1;
+                ($C[$context]{low} <<= 1) &= ((1 << (BITS - 1)) - 1);
+                ++$C[$context]{uf_count};
             }
             else {
                 last;
             }
+
+            $C[$context]{low}  &= MAX;
+            $C[$context]{high} &= MAX;
         }
     }
 
-    for (my $k = 0 ; $k <= $#{$bytes} ; ++$k) {
-
-        my $c = $bytes->[$k];
-
+    foreach my $c (@$bytes) {
         if ($C[1]{freq}{$c} == 0) {
             encode_symbol(ESCAPE, 1);
             encode_symbol($c,     0);
@@ -175,7 +162,13 @@ sub encode ($string) {
         }
     }
 
+    $enc .= '0';
     $enc .= '1';
+
+    while (length($enc) % 8 != 0) {
+        $enc .= '1';
+    }
+
     return $enc;
 }
 
@@ -185,7 +178,7 @@ sub decode ($bits) {
     my @C = create_contexts();
 
     my $dec = '';
-    my $enc = oct('0b' . join '', map { getc($fh) // 0 } 1 .. BITS);
+    my $enc = oct('0b' . join '', map { getc($fh) // 1 } 1 .. BITS);
 
     my $context = 1;
 
@@ -211,8 +204,8 @@ sub decode ($bits) {
             $dec .= chr($i);
         }
 
-        $C[$context]{high} = $C[$context]{low} + int(($w * $C[$context]{cf_high}->{$i}) / $C[$context]{T});
-        $C[$context]{low}  = $C[$context]{low} + int(($w * $C[$context]{cf_low}->{$i}) / $C[$context]{T});
+        $C[$context]{high} = ($C[$context]{low} + int(($w * $C[$context]{cf_high}->{$i}) / $C[$context]{T}) - 1) & MAX;
+        $C[$context]{low}  = ($C[$context]{low} + int(($w * $C[$context]{cf_low}->{$i}) / $C[$context]{T})) & MAX;
 
         foreach my $context (1) {
             $C[$context]{T} = increment_freq($i, $C[$context]{freq}, $C[$context]{cf_low}, $C[$context]{cf_high});
@@ -228,41 +221,24 @@ sub decode ($bits) {
 
         while (1) {
 
-            if (($C[$context]{low} & MSB) == ($C[$context]{high} & MSB)) {
-
-                if (($C[$context]{low} & MSB) == MSB) {
-                    $C[$context]{low}  ^= MSB;
-                    $C[$context]{high} ^= MSB;
-                }
-
-                $C[$context]{low}  <<= 1;
-                $C[$context]{high} <<= 1;
-                $C[$context]{high} |= 1;
-
-                if (($enc & MSB) == MSB) {
-                    $enc ^= MSB;
-                }
-
-                $enc <<= 1;
-                $enc |= getc($fh) // 0;
+            if (($C[$context]{high} >> (BITS - 1)) == ($C[$context]{low} >> (BITS - 1))) {
+                ($C[$context]{high} <<= 1) |= 1;
+                $C[$context]{low} <<= 1;
+                ($enc <<= 1) |= (getc($fh) // 1);
             }
-            elsif ((($C[$context]{low} & SMSB) == SMSB) and (($C[$context]{high} & SMSB) == 0)) {
-
-                $C[$context]{low} ^= SMSB;
-                $C[$context]{high} -= SMSB;
-
-                $enc -= SMSB if ($enc >= SMSB);
-
-                $C[$context]{low}  <<= 1;
-                $C[$context]{high} <<= 1;
-                $enc               <<= 1;
-
+            elsif (((($C[$context]{low} >> (BITS - 2)) & 0x1) == 1) && ((($C[$context]{high} >> (BITS - 2)) & 0x1) == 0)) {
+                ($C[$context]{high} <<= 1) |= (1 << (BITS - 1));
                 $C[$context]{high} |= 1;
-                $enc |= getc($fh) // 0;
+                ($C[$context]{low} <<= 1) &= ((1 << (BITS - 1)) - 1);
+                $enc = (($enc >> (BITS - 1)) << (BITS - 1)) | (($enc & ((1 << (BITS - 2)) - 1)) << 1) | (getc($fh) // 1);
             }
             else {
                 last;
             }
+
+            $C[$context]{low}  &= MAX;
+            $C[$context]{high} &= MAX;
+            $enc               &= MAX;
         }
 
         if ($i == ESCAPE) {
@@ -302,6 +278,6 @@ say $dec;
 $str eq $dec or die "Decoding error: ", length($str), ' <=> ', length($dec);
 
 __END__
-0100000100000001110010111101111100000011110011111100011111000001101110101100000100111000001011100001010000111001110111001110111100001100110111100010111001011010100110001101111010010011001110100100000010001011101111001010100110111
-Encoded bytes length: 28.625
+0100000011000001000010010011111111110001001000010100100101000010110101110111001000110110110010011001000111010101100010111110010111111101011110010010110000110100100101110011110101110111101000110000011100010111111100001010011011001011
+Encoded bytes length: 29
 ABRACADABRA AND A VERY SAD SALAD
