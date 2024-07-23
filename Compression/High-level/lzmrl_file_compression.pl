0d37b790f88177ad21d67703898c3110163d78c6
trizen <trizenx@tutanota.com>
2024-07-23 08:31:58 +0300

    new file:   Compression/High-level/lz2ss_file_compression.pl new file:   Compression/High-level/lz772_file_compression.pl new file:   Compression/High-level/lz77f_file_compression.pl new file:   Compression/High-level/lzmrl_file_compression.pl new file:   Compression/High-level/lzss2_file_compression.pl new file:   Compression/High-level/lzss77_file_compression.pl new file:   Compression/High-level/lzssm_file_compression.pl

---
+++ Compression/High-level/lzmrl_file_compression.pl
@@ -0,0 +1,152 @@
+#!/usr/bin/perl
+
+# Author: Trizen
+# Date: 15 December 2022
+# Edit: 23 July 2024
+# https://github.com/trizen
+
+# Compress/decompress files using LZ77 compression + Move-to-front + RLE + Huffman coding.
+
+use 5.036;
+
+use Getopt::Std       qw(getopts);
+use File::Basename    qw(basename);
+use Compression::Util qw(:all);
+
+use constant {
+    PKGNAME => 'LZMRL',
+    VERSION => '0.01',
+    FORMAT  => 'lzmrl',
+
+    CHUNK_SIZE => 1 << 18,    # higher value = better compression
+};
+
+# Container signature
+use constant SIGNATURE => uc(FORMAT) . chr(1);
+
+sub usage {
+    my ($code) = @_;
+    print <<"EOH";
+usage: $0 [options] [input file] [output file]
+
+options:
+        -e            : extract
+        -i <filename> : input filename
+        -o <filename> : output filename
+        -r            : rewrite output
+
+        -v            : version number
+        -h            : this message
+
+examples:
+         $0 document.txt
+         $0 document.txt archive.${\FORMAT}
+         $0 archive.${\FORMAT} document.txt
+         $0 -e -i archive.${\FORMAT} -o document.txt
+
+EOH
+
+    exit($code // 0);
+}
+
+sub version {
+    printf("%s %s\n", PKGNAME, VERSION);
+    exit;
+}
+
+sub valid_archive {
+    my ($fh) = @_;
+
+    if (read($fh, (my $sig), length(SIGNATURE), 0) == length(SIGNATURE)) {
+        $sig eq SIGNATURE || return;
+    }
+
+    return 1;
+}
+
+sub main {
+    my %opt;
+    getopts('ei:o:vhr', \%opt);
+
+    $opt{h} && usage(0);
+    $opt{v} && version();
+
+    my ($input, $output) = @ARGV;
+    $input  //= $opt{i} // usage(2);
+    $output //= $opt{o};
+
+    my $ext = qr{\.${\FORMAT}\z}io;
+    if ($opt{e} || $input =~ $ext) {
+
+        if (not defined $output) {
+            ($output = basename($input)) =~ s{$ext}{}
+              || die "$0: no output file specified!\n";
+        }
+
+        if (not $opt{r} and -e $output) {
+            print "'$output' already exists! -- Replace? [y/N] ";
+            <STDIN> =~ /^y/i || exit 17;
+        }
+
+        decompress_file($input, $output)
+          || die "$0: error: decompression failed!\n";
+    }
+    elsif ($input !~ $ext || (defined($output) && $output =~ $ext)) {
+        $output //= basename($input) . '.' . FORMAT;
+        compress_file($input, $output)
+          || die "$0: error: compression failed!\n";
+    }
+    else {
+        warn "$0: don't know what to do...\n";
+        usage(1);
+    }
+}
+
+# Compress file
+sub compress_file ($input, $output) {
+
+    open my $fh, '<:raw', $input
+      or die "Can't open file <<$input>> for reading: $!";
+
+    my $header = SIGNATURE;
+
+    # Open the output file for writing
+    open my $out_fh, '>:raw', $output
+      or die "Can't open file <<$output>> for write: $!";
+
+    # Print the header
+    print $out_fh $header;
+
+    # Compress data
+    while (read($fh, (my $chunk), CHUNK_SIZE)) {
+        print $out_fh lz77_compress($chunk, \&mrl_compress_symbolic);
+    }
+
+    # Close the file
+    close $out_fh;
+}
+
+# Decompress file
+sub decompress_file ($input, $output) {
+
+    # Open and validate the input file
+    open my $fh, '<:raw', $input
+      or die "Can't open file <<$input>> for reading: $!";
+
+    valid_archive($fh) || die "$0: file `$input' is not a \U${\FORMAT}\E v${\VERSION} archive!\n";
+
+    # Open the output file
+    open my $out_fh, '>:raw', $output
+      or die "Can't open file <<$output>> for writing: $!";
+
+    while (!eof($fh)) {
+        print $out_fh lz77_decompress($fh, \&mrl_decompress_symbolic);
+    }
+
+    # Close the file
+    close $fh;
+    close $out_fh;
+}
+
+main();
+exit(0);
