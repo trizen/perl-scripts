73aa63c166a179898f59c1ae06645b9c0a60e5e0
trizen <trizenx@tutanota.com>
2024-08-09 19:28:14 +0300

    new file:   Compression/High-level/bzip2_file_compression.pl new file:   Compression/High-level/gzip_file_compression.pl new file:   Compression/High-level/lzop_file_compression.pl new file:   Compression/High-level/xz_file_compression.pl new file:   Compression/High-level/zstd_file_compression.pl

---
+++ Compression/High-level/gzip_file_compression.pl
@@ -0,0 +1,122 @@
+#!/usr/bin/perl
+
+# Compress files using Gzip.
+
+use 5.036;
+use Getopt::Std    qw(getopts);
+use File::Basename qw(basename);
+
+use IO::Compress::Gzip     qw(gzip);
+use IO::Uncompress::Gunzip qw(gunzip);
+
+use constant {
+              PKGNAME => 'GZIP',
+              VERSION => '0.01',
+              FORMAT  => 'gz',
+             };
+
+sub usage {
+    my ($code) = @_;
+    print <<"EOH";
+usage: $0 [options] [input file] [output file]
+
+options:
+        -e            : extract
+        -i <filename> : input filename
+        -o <filename> : output filename
+        -r            : rewrite output
+
+        -v            : version number
+        -h            : this message
+
+examples:
+         $0 document.txt
+         $0 document.txt archive.${\FORMAT}
+         $0 archive.${\FORMAT} document.txt
+         $0 -e -i archive.${\FORMAT} -o document.txt
+
+EOH
+
+    exit($code // 0);
+}
+
+sub version {
+    printf("%s %s\n", PKGNAME, VERSION);
+    exit;
+}
+
+sub main {
+    my %opt;
+    getopts('ei:o:vhr', \%opt);
+
+    $opt{h} && usage(0);
+    $opt{v} && version();
+
+    my ($input, $output) = @ARGV;
+    $input  //= $opt{i} // usage(2);
+    $output //= $opt{o};
+
+    my $ext = qr{\.${\FORMAT}\z}io;
+    if ($opt{e} || $input =~ $ext) {
+
+        if (not defined $output) {
+            ($output = basename($input)) =~ s{$ext}{}
+              || die "$0: no output file specified!\n";
+        }
+
+        if (not $opt{r} and -e $output) {
+            print "'$output' already exists! -- Replace? [y/N] ";
+            <STDIN> =~ /^y/i || exit 17;
+        }
+
+        decompress_file($input, $output)
+          || die "$0: error: decompression failed!\n";
+    }
+    elsif ($input !~ $ext || (defined($output) && $output =~ $ext)) {
+        $output //= basename($input) . '.' . FORMAT;
+        compress_file($input, $output)
+          || die "$0: error: compression failed!\n";
+    }
+    else {
+        warn "$0: don't know what to do...\n";
+        usage(1);
+    }
+}
+
+# Compress file
+sub compress_file ($input, $output) {
+
+    open my $fh, '<:raw', $input
+      or die "Can't open file <<$input>> for reading: $!";
+
+    # Open the output file for writing
+    open my $out_fh, '>:raw', $output
+      or die "Can't open file <<$output>> for write: $!";
+
+    # Compress data
+    gzip($fh, $out_fh) or die "compression error";
+
+    # Close the file
+    close $out_fh;
+}
+
+# Decompress file
+sub decompress_file ($input, $output) {
+
+    # Open and validate the input file
+    open my $fh, '<:raw', $input
+      or die "Can't open file <<$input>> for reading: $!";
+
+    # Open the output file
+    open my $out_fh, '>:raw', $output
+      or die "Can't open file <<$output>> for writing: $!";
+
+    gunzip($fh, $out_fh) or die "decompression error";
+
+    # Close the file
+    close $fh;
+    close $out_fh;
+}
+
+main();
+exit(0);
