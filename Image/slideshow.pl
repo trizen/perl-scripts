ddb313d64868d058bb0683e2dd3d9fcb8a70114c
trizen <trizenx@tutanota.com>
2024-09-21 08:38:02 +0300

    renamed:    Converters/slideshow.pl -> Image/slideshow.pl

--- Image/slideshow.pl
+++ Image/slideshow.pl
@@ -7,10 +7,12 @@
 
 use 5.036;
 
+@ARGV == 2 or die "usage: $0 [glob pattern] [output.mp4]\n";
+
 system('ffmpeg',
        qw(-framerate 1/2),
        qw(-pattern_type glob -i),
        $ARGV[0], '-vf',
        "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1929:1080:(ow-iw)/2:(oh-ih)/2",
-       qw(-tune stillimage -c:v libx264 -s 1920x1080 -crf 18 -r 24),
+       qw(-c:v libx264 -s 1920x1080 -crf 18 -tune stillimage -r 24),
        $ARGV[1]);
